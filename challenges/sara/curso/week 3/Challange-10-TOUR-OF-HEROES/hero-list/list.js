44c83201626175edb6a37c6e84ba718b424c2f45
Sara Cubero Encinas <sara@MacBook-Pro-de-Sara.local>
2021-04-26 19:07:21 +0200

    adding onclick to HTMLs

--- challenges/sara/curso/week 3/Challange-10-TOUR-OF-HEROES/hero-list/list.js
+++ challenges/sara/curso/week 3/Challange-10-TOUR-OF-HEROES/hero-list/list.js
@@ -2,18 +2,24 @@ let dashButton = document.querySelector(".dashboard__button");
 let listButton = document.querySelector(".list__button");
 let mainContainerList = document.querySelector(".list-main__container--first");
 
-dashButton.addEventListener("click", () => {
+function getToDashBoard() {
   window.location.href = "http://127.0.0.1:5500/dashboard/dashboard.html";
-});
+}
 
-listButton.addEventListener("click", () => {
+function getToList() {
   window.location.href = "http://127.0.0.1:5500/hero-list/list.html";
-});
+}
+
+function createAnchor(index) {
+  let newAnchor = document.createElement("a");
+  newAnchor.innerText = HEROES[i]["id"] + " " + HEROES[i]["name"];
+}
 
 function createTheList() {
-  for (let i = 0; i < HEROES.lenght; i++) {
-    let newAnchor = document.createElement("a");
+  for (let i = 0; i < HEROES.length; i++) {
+    let newAnchor = createTheList(i);
     mainContainerList.appendChild(newAnchor);
-    newAnchor.innerText = "hola";
   }
 }
+
+createTheList();

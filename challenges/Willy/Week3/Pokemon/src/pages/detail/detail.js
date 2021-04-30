453b8a0cc3762a08fb5ce4a8a8958cb736244ef2
Willy <acropolis_arcaica@hotmail.com>
2021-04-30 12:18:50 +0200

    adding music

--- challenges/Willy/Week3/Pokemon/src/pages/detail/detail.js
+++ challenges/Willy/Week3/Pokemon/src/pages/detail/detail.js
@@ -4,12 +4,21 @@
     const pokeRef = window.location.search.substr(6);
     const pokeUrl =  `https://pokeapi.co/api/v2/pokemon/${pokeRef}`
     const getPokeFetch = await getPokemon(pokeUrl)
-    console.log(getPokeFetch)
+    
     const PokeContainer = document.getElementById('pokemon__container') 
     const pokeDetail = document.createElement('p')
     pokeDetail.innerText = `\tId: ${getPokeFetch.id} \n\tName: ${getPokeFetch.name} \n\tHeight: ${getPokeFetch.height} \n\tWeight: ${getPokeFetch.weight}`;
     pokeImg = document.createElement('img');
+    pokeImg.id = "pokeSprite";
     pokeImg.src = getPokeFetch.sprites.front_default;
+
+    pokeImg.onmouseenter = function(){
+      pokeImg.src = getPokeFetch.sprites.back_default;
+    }
+
+    pokeImg.onmouseout = function(){
+      pokeImg.src = getPokeFetch.sprites.front_default;
+    }
     
     PokeContainer.appendChild(pokeDetail);
     PokeContainer.appendChild(pokeImg);
--- challenges/Willy/Week3/Pokemon/src/pages/list/list.js
+++ challenges/Willy/Week3/Pokemon/src/pages/list/list.js
@@ -1,16 +1,17 @@
 (function listPage() {
-    getPokes(10,0).then(pokeResponse => {
+    getPokes(20,0).then(pokeResponse => {
         console.log(pokeResponse)
 
        // const [countSpan] = document.getElementsByClassName('total-pokes')
         //countSpan.textContent = pokeResponse.count;
         
-        const pokeList = document.getElementById('poke-list')   
+        const pokeList = document.getElementById('poke__list')   
         pokeResponse.results.forEach(poke => {
             const element = document.createElement('a')
+            element.classList.add('poke__list--pokemon') 
             element.href = `http://127.0.0.1:5500/src/pages/detail/detail.html?name=${poke.name}` 
             element.innerText = poke.name
-            document.body.appendChild(element);
+            pokeList.appendChild(element);
         })
         
       

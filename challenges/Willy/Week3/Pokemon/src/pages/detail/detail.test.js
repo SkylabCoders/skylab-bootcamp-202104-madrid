da5e94b418ea7a92acbfe4c40849a43a588d1715
Willy <acropolis_arcaica@hotmail.com>
2021-04-30 17:29:54 +0200

    adding a class

---
+++ challenges/Willy/Week3/Pokemon/src/pages/detail/detail.test.js
@@ -0,0 +1,9 @@
+
+const detail = require('./detail')
+
+describe('given a detail class', () => {
+    describe('when is invoked', () => {
+        const page = new Detail()
+        test(`then return an object`)
+    })
+})

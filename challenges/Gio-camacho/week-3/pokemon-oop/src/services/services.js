7903b014e4947dfc47d4673ef100b80af67bdf4b
Renszo Camacho <varsovia84@gmail.com>
2021-05-01 13:22:12 +0200

    Adding getAllPokemons method with class ant testing

--- challenges/Gio-camacho/week-3/pokemon-oop/src/services/services.js
+++ challenges/Gio-camacho/week-3/pokemon-oop/src/services/services.js
@@ -1,12 +1,14 @@
 class Services {
-  constructor(name) {
+  constructor(name, limit, offset) {
     this.name = name;
+    this.limit = limit;
+    this.offset = offset;
   }
 
-  // getAllPokemons() {
-  //   return fetch(`https://pokeapi.co/api/v2/pokemon?${this.limit}=0&${this.offset}=100`)
-  //     .then((response) => response.json());
-  // }
+  getAllPokemons() {
+    return fetch(`https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${this.offset}`)
+      .then((response) => response.json());
+  }
 
   getPokemon() {
     return fetch(`https://pokeapi.co/api/v2/pokemon/${this.name}`)
--- challenges/Gio-camacho/week-3/pokemon-oop/src/services/services.test.js
+++ challenges/Gio-camacho/week-3/pokemon-oop/src/services/services.test.js
@@ -1,8 +1,8 @@
 const Services = require('./services');
 
-describe('Given an new Object', () => {
-  describe('When the new Object call the getPokemon method', () => {
-    test('Then it should return an object', async () => {
+describe('Given a getPokemon Object Method', () => {
+  describe('When the new Object call the getPokemon() method', () => {
+    test('The should return an object {name: "ditto"}', async () => {
       global.fetch = jest.fn().mockReturnValueOnce(
         Promise.resolve({
           json: jest.fn().mockReturnValueOnce(
@@ -12,9 +12,40 @@ describe('Given an new Object', () => {
           )
         })
       );
-      const service = new Services();
-      const pokemon = await service.getPokemon('ditto');
+
+      const services = new Services();
+      const pokemon = await services.getPokemon('ditto');
       expect(pokemon).toEqual({ name: 'ditto' });
+    });
+  });
+});
+
+describe('Given an getAllPokemons Object Method', () => {
+  describe('When getAllPokemons is invoked', () => {
+    describe('And passing a beforeEach', () => {
+      let allPokemons;
+
+      beforeEach(async () => {
+        global.fetch = jest.fn().mockReturnValueOnce(
+          Promise.resolve({
+            json: jest.fn().mockReturnValueOnce(
+              Promise.resolve({
+                count: 1118,
+                next: 'limit=10',
+                previous: null,
+                result: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+              })
+            )
+          })
+        );
+
+        const allServices = new Services();
+        allPokemons = await allServices.getAllPokemons(null, 10, 0);
+      });
+
+      test('Then should return ...', () => {
+        expect(allPokemons.count).toBe(1118);
+      });
     });
   });
 });

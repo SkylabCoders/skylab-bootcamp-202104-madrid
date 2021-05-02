31b67b06a8e6e78ba32e53ebcf1e02d64cac1e50
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-02 20:29:21 +0200

    Fix header on top

--- challenges/carlos-lopez/week3/pokemon/src/pages/dashboard/dashboard.js
+++ challenges/carlos-lopez/week3/pokemon/src/pages/dashboard/dashboard.js
@@ -18,6 +18,7 @@ class CreateTopPokemons {
         newAnchor.appendChild(pokeImg);
         const pokeName = document.createElement('span');
         pokeName.innerHTML = poke.name;
+        newAnchor.href = `http://127.0.0.1:5500/src/pages/detail/detail.html?name=${poke.name}`;
         newAnchor.appendChild(pokeName);
       });
     }

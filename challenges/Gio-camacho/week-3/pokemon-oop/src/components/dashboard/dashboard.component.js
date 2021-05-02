841a84c4232c34e5a56a7e85fff5f0d655180345
Renszo Camacho <varsovia84@gmail.com>
2021-05-02 18:18:59 +0200

    Styling the dashboard navbar

--- challenges/Gio-camacho/week-3/pokemon-oop/src/components/dashboard/dashboard.component.js
+++ challenges/Gio-camacho/week-3/pokemon-oop/src/components/dashboard/dashboard.component.js
@@ -1,15 +1,31 @@
-// const Helper = require('../../services/helper');
-
 (function () {
   const wrapperDashboard = document.querySelector('.wrapper-dashboard');
   const limit = 20;
   let offset;
   offset = Math.floor(Math.random() * 100) + 1;
 
-  const title = new Helper('h1');
-  const h1 = title.htmlTagGenerator('PokeDashboard', 'wrapper-dashboard__title');
-  wrapperDashboard.appendChild(h1);
+  /* Making a navbar */
+  const navbar = new Helper('nav');
+  const nav = navbar.htmlTagGenerator(null, 'wrapper-dashboard__navbar');
+  wrapperDashboard.appendChild(nav);
 
+  const title = new Helper('h1');
+  const h1 = title.htmlTagGenerator('Dashboard', 'wrapper-dashboard__title');
+  nav.appendChild(h1);
+
+  const navbarList = new Helper('ul');
+  const navbarUl = navbarList.htmlTagGenerator(null, 'wrapper-dashboard__navbar-list');
+  nav.appendChild(navbarUl);
+
+  const navbarItem = new Helper('li');
+  const navbarLi = navbarItem.htmlTagGenerator(null, 'wrapper-dashboard__navbar-item');
+  navbarUl.appendChild(navbarLi);
+
+  const navbarLink = new Helper('a');
+  const navbarListAnchor = navbarLink.htmlTagGenerator('PokeList', 'wrapper-dashboard__navbar-link', 'http://127.0.0.1:5500/src/components/list/list.html');
+  navbarLi.appendChild(navbarListAnchor);
+
+  /* Rendering PokeList */
   const services = new Services(null, limit, offset);
   services.getAllPokemons()
 
@@ -21,8 +37,6 @@
       wrapperDashboard.appendChild(ul);
 
       randomPokemons.slice(0, 4).forEach((pokemon) => {
-        console.log(pokemon);
-
         const item = new Helper('li');
         const li = item.htmlTagGenerator(null, 'wrapper-dashboard__item', null, null);
         ul.appendChild(li);

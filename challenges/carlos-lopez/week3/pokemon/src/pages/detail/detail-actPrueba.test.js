cd7424cfd4a6c4f35c338fb7c01ab865f7f4e8ba
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-02 14:01:17 +0200

    Add new test mocking fetch

--- challenges/carlos-lopez/week3/pokemon/src/pages/detail/detail-actPrueba.test.js
+++ challenges/carlos-lopez/week3/pokemon/src/pages/detail/detail-actPrueba.test.js
@@ -4,26 +4,42 @@ const { getPokes, getPokemon } = require('../../services/pokemon');
 describe('Given a method paintTopPokemons', () => {
   describe('When calling it', () => {
     const newLocal = 'https://pokeapi.co/api/v2/pokemon?limit=1118&offset=0';
-    const pokemon = new CreateTopPokemons(newLocal);
+    let pokemon = new CreateTopPokemons(newLocal);
     const anchor = document.createElement('a');
     anchor.className = 'dashboard__poke';
     const pokeImg = document.createElement('img');
+    const pokeName = document.createElement('span');
+
+    beforeEach(async () => {
+      global.fetch = jest.fn().mockReturnValueOnce(
+        Promise.resolve({
+          json: jest.fn()
+            .mockReturnValueOnce(Promise.resolve({
+              id: 7, height: 5, weight: 90, name: 'squirtle', img: 'http://pokeapi.it/',
+            })),
+        }),
+      );
+      pokemon = await getPokemon();
+    });
 
     test('Then it should contain a new anchor', () => {
       document.body.innerHTML = '<a class="dashboard__poke"></a>';
       expect(document.body.innerHTML).toBe('<a class="dashboard__poke"></a>');
     });
 
-    test('Then it should contain a new img tag', () => {
+    test('Then the anchor should contain a new img tag', () => {
       document.body.innerHTML = '<a class="dashboard__poke"></a>';
       const dashboardAnchor = document.querySelector('.dashboard__poke');
       dashboardAnchor.appendChild(pokeImg);
       expect(dashboardAnchor.innerHTML).toContain('<img>');
     });
-    test('Then it should contain a new img', () => {
+    test('Then the new img tag has a src ="http://pokeapi.it/" ', () => {
       pokeImg.src = 'http://pokeapi.it';
-      document.body.innerHTML = '<img src="http://pokeapi.it">';
-      expect(document.body.innerHTML).toBe('<img src="http://pokeapi.it">');
+      expect(pokeImg.src).toBe(pokemon.img);
+    });
+    test('Then the new span tag has the name of squirtle" ', () => {
+      pokeName.innerHTML = pokemon.name;
+      expect(pokeName.innerHTML).toContain('squirtle');
     });
   });
 });

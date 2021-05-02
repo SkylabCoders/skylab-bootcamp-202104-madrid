978564a64371c4f62fcd74af841bde01af522b78
Renszo Camacho <varsovia84@gmail.com>
2021-05-02 13:28:55 +0200

    Pagination - Adding functionality to the buttons next and previous

--- challenges/Gio-camacho/week-3/pokemon-oop/src/components/list/list.component.js
+++ challenges/Gio-camacho/week-3/pokemon-oop/src/components/list/list.component.js
@@ -1,15 +1,19 @@
 (function () {
+  const limit = 10;
+  let offset;
+  offset = 0;
+
   const wrapperList = document.querySelector('.wrapper-list');
 
   const title = new Helper('h1');
-  const h1 = title.htmlTagGenerator('Pokemon', 'wrapper-list__title', null, null);
+  const h1 = title.htmlTagGenerator('Pokemons List', 'wrapper-list__title', null, null);
   wrapperList.appendChild(h1);
 
   const list = new Helper('ul');
   const ul = list.htmlTagGenerator(null, 'wrapper-list__list', null, null);
   wrapperList.appendChild(ul);
 
-  const pokemons = new Services(null, 10, 0);
+  const pokemons = new Services(null, limit, offset);
 
   pokemons.getAllPokemons().then((dataList) => {
     dataList.results.forEach((pokemon) => {
@@ -30,4 +34,52 @@
 
   const buttonPrevious = btn.htmlTagGenerator('Previous', 'wrapper-list__previous', null, null);
   wrapperList.appendChild(buttonPrevious);
+
+  buttonNext.addEventListener('click', () => {
+    if (offset >= 0) {
+      offset += limit;
+    }
+
+    const nextPagination = new Services(null, limit, offset);
+
+    nextPagination.getAllPokemons()
+      .then((dataList) => {
+        ul.innerHTML = '';
+
+        dataList.results.forEach((pokemon) => {
+          const item = new Helper('li');
+          const li = item.htmlTagGenerator(null, 'wrapper-list__item', null, null);
+
+          const link = new Helper('a');
+          const a = link.htmlTagGenerator(pokemon.name, 'wrapper-list__link', `http://127.0.0.1:5500/src/components/details/detalles.html?name=${pokemon.name}`, null);
+
+          li.appendChild(a);
+          ul.appendChild(li);
+        });
+      });
+  });
+
+  buttonPrevious.addEventListener('click', () => {
+    if (offset >= 10) {
+      offset -= limit;
+    }
+
+    const previousPagination = new Services(null, limit, offset);
+
+    previousPagination.getAllPokemons()
+      .then((dataList) => {
+        ul.innerHTML = '';
+
+        dataList.results.forEach((pokemon) => {
+          const item = new Helper('li');
+          const li = item.htmlTagGenerator(null, 'wrapper-list__item', null, null);
+
+          const link = new Helper('a');
+          const a = link.htmlTagGenerator(pokemon.name, 'wrapper-list__link', `http://127.0.0.1:5500/src/components/details/detalles.html?name=${pokemon.name}`, null);
+
+          li.appendChild(a);
+          ul.appendChild(li);
+        });
+      });
+  });
 }());

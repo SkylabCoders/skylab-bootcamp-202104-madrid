c0aa3a0637c29be4e31103c7ef0af4f2faf38231
Adriancit0 <adrianruanol@gmail.com>
2021-05-02 17:03:16 +0200

    add pagination numbers

--- challenges/adrian/Curso/week-3/Pokemon/src/pages/detail/detail-class-act.js
+++ challenges/adrian/Curso/week-3/Pokemon/src/pages/detail/detail-class-act.js
@@ -3,6 +3,7 @@ const pokeName = params.get('name');
 
 (function () {
   let currentPokemon;
+  console.log(currentPokemon);
   getPokemon(`https://pokeapi.co/api/v2/pokemon/${pokeName}`).then((pokemon) => {
     currentPokemon = new DetailPages(pokemon);
     currentPokemon.drawName();
--- challenges/adrian/Curso/week-3/Pokemon/src/pages/list/list.js
+++ challenges/adrian/Curso/week-3/Pokemon/src/pages/list/list.js
@@ -1,26 +1,7 @@
-/* (function listPage() {
-  getPokes(10, 0).then((pokeResponse) => {
-    const [countSpan] = document.getElementsByClassName('total-pokes');
-    countSpan.textContent = pokeResponse.count;
-
-    const pokeList = document.getElementById('poke-list');
-
-    pokeResponse.results.forEach((poke) => {
-      const element = document.createElement('li');
-      const anchor = document.createElement('a');
-      anchor.innerText = poke.name;
-      anchor.href = `http://127.0.0.1:5500/challenges/adrian/Curso/week-3/Pokemon/src/pages/detail/detail.html?name=${poke.name}`;
-      element.appendChild(anchor);
-      pokeList.appendChild(element);
-    });
-  });
-}());
-*/
-
 const buttonPrev = document.querySelector('.prev');
 const buttonNext = document.querySelector('.next');
 let thisTimePokemons;
-function listPage(url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0') {
+function listPage(url = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0') {
   getPokes(url).then((pokeResponse) => {
     thisTimePokemons = pokeResponse;
     const pokeList = document.getElementById('poke-list');
@@ -31,23 +12,33 @@ function listPage(url = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0') {
       pokeList.appendChild(listElement);
       listElement.appendChild(element);
       element.innerHTML = poke.name;
-      element.href = `http://127.0.0.1:5500/src/pages/detail/detail.html?name=${poke.name}`;
+      element.href = `http://127.0.0.1:5500/challenges/adrian/Curso/week-3/Pokemon/src/pages/detail/detail.html?name=${poke.name}`;
     });
   });
 }
 
 buttonPrev.addEventListener('click', () => {
   const newUrl = thisTimePokemons.previous;
-  if (newUrl !== 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0') {
+  if (newUrl !== 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0') {
     return listPage(newUrl);
   }
 });
 buttonNext.addEventListener('click', () => {
   console.log('next');
   const newUrl = thisTimePokemons.next;
-  console.log(newUrl);
-  debugger;
   return listPage(newUrl);
+});
+
+const paginationNumbers = document.getElementsByClassName('pages');
+
+paginationNumbers.forEach((number) => {
+  number.addEventListener('click', (event) => {
+    paginationNumbers.forEach((e) => { e.className = 'pages'; });
+    number.className = 'thisTimePage';
+    const offset = event.target.id;
+    const newUrl = `https://pokeapi.co/api/v2/pokemon?limit=100&offset=${offset}`;
+    return listPage(newUrl);
+  });
 });
 
 listPage();

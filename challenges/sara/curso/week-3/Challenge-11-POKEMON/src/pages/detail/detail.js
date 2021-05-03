13d713f190e2f024fd02f076393e89d01dc74a38
Sara Cubero Encinas <sara@MacBook-Pro-de-Sara.local>
2021-05-03 17:13:56 +0200

    fix css and start test

--- challenges/sara/curso/week-3/Challenge-11-POKEMON/src/pages/detail/detail.js
+++ challenges/sara/curso/week-3/Challenge-11-POKEMON/src/pages/detail/detail.js
@@ -10,12 +10,15 @@ class DetailPage {
 
   paintImg() {
     const pokeImg = document.getElementById('poke-img');
+    const pokeImgBack = document.getElementById('poke-img-back');
+    pokeImgBack.setAttribute('src', this.pokemon.sprites.back_default);
     pokeImg.setAttribute('src', this.pokemon.sprites.front_default);
   }
 
   addAditionalInfo() {
     const newDiv = document.createElement('div');
-    document.body.appendChild(newDiv);
+    const main = document.querySelector('main');
+    main.appendChild(newDiv);
     newDiv.innerText = `\t- Height: ${this.pokemon.height} cm \n\t- Weight: ${this.pokemon.weight} kg`;
   }
 }
---
+++ challenges/sara/curso/week-3/Challenge-11-POKEMON/src/pages/list/list.test.js
@@ -0,0 +1,1 @@
+test('it needs to be done', () => {});

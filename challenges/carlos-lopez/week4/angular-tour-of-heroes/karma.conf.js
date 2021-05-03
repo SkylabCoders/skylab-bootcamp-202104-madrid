983f0bfa693583a3ad7bf764f926f4ecdce1b257
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-03 18:15:32 +0200

    Create new Workspace

--- challenges/carlos-lopez/week4/angular-tour-of-heroes/karma.conf.js
+++ challenges/carlos-lopez/week4/angular-tour-of-heroes/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pruebaAngular'),
+      dir: require('path').join(__dirname, './coverage/angular-tour-of-heroes'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- challenges/carlos-lopez/week4/angular-tour-of-heroes/src/app/app.component.spec.ts
+++ challenges/carlos-lopez/week4/angular-tour-of-heroes/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pruebaAngular'`, () => {
+  it(`should have as title 'angular-tour-of-heroes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pruebaAngular');
+    expect(app.title).toEqual('angular-tour-of-heroes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pruebaAngular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heroes app is running!');
   });
 });

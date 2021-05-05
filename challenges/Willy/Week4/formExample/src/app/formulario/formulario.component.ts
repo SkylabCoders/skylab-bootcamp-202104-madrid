ba60702b43a9c53e2318bc5f9c6db3c17a241609
Willy <acropolis_arcaica@hotmail.com>
2021-05-05 16:05:51 +0200

    add services for API

---
+++ challenges/Willy/Week4/formExample/src/app/formulario/formulario.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from './../formClass'
+import { Character } from './../formClass'
+import { MainService } from './../services/main.service'
+import { HttpService} from './../services/http.service'
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.scss']
+})
+export class FormularioComponent implements OnInit {
+
+
+  model = new Person(this.mainSrv.getName(), 'Skywalker', 19,); //asi se llama a los servicios
+  url = 'https://swapi.dev/api/people';
+  
+  constructor(private mainSrv: MainService, private httpSrv: HttpService) { }
+
+  onSubmit() { 
+  
+   console.log(this.model)
+
+   }
+
+  newCharacter() {
+    this.model = new Person('', '', 0);
+    console.log(this.model)
+  }
+
+  ngOnInit(): void {
+  }
+
+  getData(){
+    this.httpSrv.getData(this.url).then(res => console.log(res))
+  }
+
+}

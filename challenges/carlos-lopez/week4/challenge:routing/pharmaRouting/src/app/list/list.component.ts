f32e81f14c84d365b1a67b936bfad5aa810cd229
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-06 21:18:18 +0200

    Improving form

--- challenges/carlos-lopez/week4/challenge:routing/pharmaRouting/src/app/list/list.component.ts
+++ challenges/carlos-lopez/week4/challenge:routing/pharmaRouting/src/app/list/list.component.ts
@@ -7,18 +7,25 @@ import { ComunicationService } from '../services/comunication.service';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  products = [
-    {name: 'PURITO - Limpiador Defence Barrier pH Cleanser 150 ml', price: 11.70, inTheBasket: false},
-    {name: 'THE ORDINARY - Niacinamide 10% Zinc 1%', price: 5.75, inTheBasket: false},
-    {name: 'iUNIK - Crema en gel calmante con centella 60 ml', price: 9.50, inTheBasket: false},
-  ]
+  model = {name:"", price: 0, inTheBasket:false};
   
   constructor(public srv:ComunicationService) { }
 
   ngOnInit(): void {
   }
   addToBuy(item:any){
-    this.srv.basket.push(item);
-  };
+    if(!item.inTheBasket){
+      item.inTheBasket = true;
+      this.srv.basket.push(item);
+      this.srv.total = this.srv.total + item.price;
+    }
+  }
+
+  onSubmit(){
+    let newModel = {...this.model};
+    this.srv.products.push(newModel);
+  }
+
+
 
 }

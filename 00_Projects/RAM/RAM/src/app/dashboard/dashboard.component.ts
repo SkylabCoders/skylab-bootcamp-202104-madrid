f69ecd39582735075b0761c3ff858907b6fc38d9
Adan <chema6822@gmail.com>
2021-05-10 19:51:28 +0200

    set dashboard visualization

--- 00_Projects/RAM/RAM/src/app/dashboard/dashboard.component.ts
+++ 00_Projects/RAM/RAM/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {StorageService} from '../services/storage.service'
 import {LoginService} from '../services/login.service'
 import {HttpService} from '../services/http.service'
+import { RickMock } from '../services/mocking/interfaceMock';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,11 +11,13 @@ import {HttpService} from '../services/http.service'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(public srvHttp:HttpService, public srvStorage: StorageService, public srvLogin: LoginService){
+  constructor(public srvHttp:HttpService, public srvStorage: StorageService, public srvLogin: LoginService) {
 
   }
+ 
 
   ngOnInit(): void {
+
   }
 
 }

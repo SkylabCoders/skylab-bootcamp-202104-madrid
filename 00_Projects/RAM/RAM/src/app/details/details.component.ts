9c27385702f2405941e9d52c346cb70ba43f8a7b
Aday <aday69@hotmail.com>
2021-05-11 17:50:13 +0200

    create details mock

--- 00_Projects/RAM/RAM/src/app/details/details.component.ts
+++ 00_Projects/RAM/RAM/src/app/details/details.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { MainService } from '../services/main.service'
+import { Component, OnInit,AfterViewInit } from '@angular/core';
+import { MainService } from '../services/main.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, AfterViewInit {
 
   url = 'https://rickandmortyapi.com/api/character';
 
   ram: any[] = [];
+
+  characterDetails = {
+    created: '2017-11-04T18:48:46.250Z',
+    episode: ['https://rickandmortyapi.com/api/episode/1', 'https://rickandmortyapi.com/api/episode/2'],
+    gender: 'Male',
+    id: 1,
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    location: {
+      name: 'Earth (Replacement Dimension)',
+      url: 'https://rickandmortyapi.com/api/location/20'
+    },
+    name: 'Rick Sanchez',
+    origin: {
+      name: 'Earth (C-137)',    
+      url: 'https://rickandmortyapi.com/api/location/1'
+    },
+    species: 'Human',
+    status: 'Alive',
+    type: '',
+    url: 'https://rickandmortyapi.com/api/character/1'
+  } 
 
   constructor(public srvMain:MainService){
 
@@ -22,6 +43,15 @@ export class DetailsComponent implements OnInit {
       console.log(this.ram);
       obs$.unsubscribe();
     })
+  }
+
+  ngAfterViewInit(){
+    const el:any = document.querySelector('.example-header-image');
+    el.style.backgroundImage =`url(${this.characterDetails.image})`;
+  }
+
+  prueba(){
+    alert('Los Fruittis mandan!');
   }
 
 }

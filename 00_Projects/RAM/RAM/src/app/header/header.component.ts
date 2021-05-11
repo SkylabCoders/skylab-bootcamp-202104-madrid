f497967ac311e5f5be67dc89e593287740bc7076
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-11 15:38:47 +0200

    Style login mobile

--- 00_Projects/RAM/RAM/src/app/header/header.component.ts
+++ 00_Projects/RAM/RAM/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { MainService } from '../services/main.service';
+import { User } from '../services/mocking/user'
 
 @Component({
   selector: 'app-header',
@@ -10,10 +12,20 @@ export class HeaderComponent implements OnInit {
   listTag = 'List';
   favoriteTag = 'Favourites';
   logIn = 'LogIn'
+  showLogIn = false;
+  model = new User(' ', ' ');
 
-  constructor () { }
+  constructor (public srvMain:MainService) { }
 
   ngOnInit (): void {
 
   }
+  openLogIn(){
+    this.showLogIn = !this.showLogIn
+  }
+  onSubmit () {
+    this.srvMain.currentUser = this.model
+    console.log(this.srvMain.currentUser)
+  }
+
 }

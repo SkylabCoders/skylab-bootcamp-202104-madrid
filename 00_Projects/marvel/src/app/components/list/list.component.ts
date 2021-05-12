401c790577793fc0230ed77f1853a9330b80a410
Willy <acropolis_arcaica@hotmail.com>
2021-05-12 11:45:13 +0200

    Create a export constant with URLs Refactor list Refactor dashboard

--- 00_Projects/marvel/src/app/components/list/list.component.ts
+++ 00_Projects/marvel/src/app/components/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { MainService } from 'src/app/services/main.service'
 import { Imarvel } from '../../models/Imarvel'
+import { URL } from '../../models/url'
 
 @Component({
   selector: 'app-list',
@@ -10,15 +11,11 @@ import { Imarvel } from '../../models/Imarvel'
 export class ListComponent implements OnInit {
    title:string = 'Character list'
    marvelList:Imarvel [] = []
-   PUBLIC_KEY = '15e7eedc86b57ed8c9aa86e4c26e4a2b'
-   HASH = '55ab7706c5f814004ae2a053827b7004'
-   readonly mainUrl= 'https://gateway.marvel.com:443/v1/public/'
-   charactersUrl = `characters?apikey=${this.PUBLIC_KEY}&hash=${this.HASH}`
 
    constructor (public mainSrv:MainService) {}
 
    ngOnInit (): void {
-     this.mainSrv.getAction('getList', (this.mainUrl + this.charactersUrl)).subscribe((res:any) => {
+     this.mainSrv.getAction('getList', (URL.apiURL + URL.CharactersURL)).subscribe((res:any) => {
        this.marvelList = res.data.results
      })
    }

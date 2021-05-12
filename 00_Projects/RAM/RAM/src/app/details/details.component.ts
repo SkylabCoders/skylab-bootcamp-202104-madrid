979d8ea8a3d7c838cbb28fd7365f7079b34fbe8f
Aday <aday69@hotmail.com>
2021-05-12 18:28:07 +0200

    Fix merge problems

--- 00_Projects/RAM/RAM/src/app/details/details.component.ts
+++ 00_Projects/RAM/RAM/src/app/details/details.component.ts
@@ -16,30 +16,6 @@ export class DetailsComponent implements OnInit, AfterViewInit {
 
   change = false;
 
-<<<<<<< HEAD
-  // characterDetails = {
-  //   created: '2017-11-04T18:48:46.250Z',
-  //   episode: ['https://rickandmortyapi.com/api/episode/1', 'https://rickandmortyapi.com/api/episode/2'],
-  //   gender: 'Male',
-  //   id: 1,
-  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-  //   location: {
-  //     name: 'Earth (Replacement Dimension)',
-  //     url: 'https://rickandmortyapi.com/api/location/20'
-  //   },
-  //   name: 'Rick Sanchez',
-  //   origin: {
-  //     name: 'Earth (C-137)',    
-  //     url: 'https://rickandmortyapi.com/api/location/1'
-  //   },
-  //   species: 'Human',
-  //   status: 'Alive',
-  //   type: '',
-  //   url: 'https://rickandmortyapi.com/api/character/1'
-  // } 
-
-=======
->>>>>>> features/project/RAM
   characterDetails = this.srvMain.detailsCharacter;
   
   constructor(public srvMain:MainService){

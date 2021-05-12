d86571b550586b7e49915266a4ff353dcf05431b
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-12 10:30:54 +0200

    Finish functionality of next button

--- 00_Projects/RAM/RAM/src/app/list/list.component.ts
+++ 00_Projects/RAM/RAM/src/app/list/list.component.ts
@@ -10,6 +10,9 @@ import { MainService } from '../services/main.service'
 export class ListComponent implements OnInit {
 
   url = 'https://rickandmortyapi.com/api/character';
+  pruebaurl = 'https://rickandmortyapi.com/api/character/?page='
+  contador = 2;
+  maxPage:any;
 
   ram: any[] = [];
 
@@ -20,8 +23,20 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
     const obs$ = this.srvMain.getTheAPI(this.url).subscribe((res:any) => {
       this.ram = res.results;
+      this.maxPage = res['info'].pages;
+      console.log(this.ram);
       obs$.unsubscribe();
     })
+  }
+
+  getNextPage():void {
+    if(this.contador <= this.maxPage){
+      const obs$ = this.srvMain.getTheAPI(`${this.pruebaurl}${this.contador}`).subscribe((res:any) => {
+        this.ram = res.results;
+        obs$.unsubscribe();
+      })
+      this.contador++
+    }
   }
 
 }
--- 00_Projects/RAM/RAM/src/app/services/storage.service.ts
+++ 00_Projects/RAM/RAM/src/app/services/storage.service.ts
@@ -5,6 +5,5 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-
   constructor() { }
 }

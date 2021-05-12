bfffcb1b2da514ea8435a8a57a9686a86c4f3341
Adriancit0 <adrianruanol@gmail.com>
2021-05-12 16:01:51 +0200

    Fix bug

--- 00_Projects/marvel/src/app/models/url.ts
+++ 00_Projects/marvel/src/app/models/url.ts
@@ -1,6 +1,8 @@
+// export
+
 // const URL: {} = {
 //   PUBLIC_KEY: '15e7eedc86b57ed8c9aa86e4c26e4a2b',
 //   HASH: '55ab7706c5f814004ae2a053827b7004',
-//   //mainUrl: 'https://gateway.marvel.com:443/v1/public/',
-//   //charactersUrl: `characters?apikey=${PUBLIC_KEY}&hash=${HASH}`
+//   mainUrl: 'https://gateway.marvel.com:443/v1/public/',
+//   charactersUrl: 'characters?apikey=15e7eedc86b57ed8c9aa86e4c26e4a2b&hash=55ab7706c5f814004ae2a053827b7004'
 // }

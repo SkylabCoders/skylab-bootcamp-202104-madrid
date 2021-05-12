7cc3e51e16efb8ce3c2244d583782a5c532e69b3
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-12 18:04:16 +0200

    Fix empty space problem

--- 00_Projects/RAM/RAM/src/app/details/details.component.ts
+++ 00_Projects/RAM/RAM/src/app/details/details.component.ts
@@ -15,10 +15,7 @@ export class DetailsComponent implements OnInit, AfterViewInit {
   favorites: any[] = [];
 
   change = false;
-<<<<<<< HEAD
 
-=======
->>>>>>> features/project/RAM
   characterDetails = this.srvMain.detailsCharacter;
   
   constructor(public srvMain:MainService){

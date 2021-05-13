3b8e0e748b30e6227eda11d5bdbb93798811012c
Sara Cubero Encinas <sara@MacBook-Pro-de-Sara.local>
2021-05-13 12:52:37 +0200

    fix search input

--- 00_Projects/RAM/RAM/src/app/header/header.component.ts
+++ 00_Projects/RAM/RAM/src/app/header/header.component.ts
@@ -33,6 +33,7 @@ export class HeaderComponent implements OnInit {
       obs$.unsubscribe();
       this.srvMain.goToList("https://rickandmortyapi.com/api/character/?name=" + this.searchInput)
       this.router.navigate(['/list'])
+      this.searchInput = ''
     }, (err:any) => {
     })
   }

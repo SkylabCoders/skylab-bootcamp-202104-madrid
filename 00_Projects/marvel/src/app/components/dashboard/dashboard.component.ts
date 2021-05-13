0c22f38c9142a002aaf77521268f417423288cb6
Renszo Camacho <varsovia84@gmail.com>
2021-05-13 09:55:31 +0200

    Modify styles in Details Component

--- 00_Projects/marvel/src/app/components/dashboard/dashboard.component.ts
+++ 00_Projects/marvel/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Imarvel } from '../../models/Imarvel';
-import { MainService } from '../../services/main.service';
-import { URL } from '../../models/url';
+import { Component, OnInit } from '@angular/core'
+import { Imarvel } from '../../models/Imarvel'
+import { MainService } from '../../services/main.service'
+import { URL } from '../../models/url'
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
   title = 'Meet de mighty heroes and villains of Marvel Universe';
@@ -14,20 +14,20 @@ export class DashboardComponent implements OnInit {
   randomList: Imarvel[] = [];
   topHeros: Imarvel[] = [];
 
-  constructor(public mainSrv: MainService) {}
+  constructor (public mainSrv: MainService) {}
 
-  ngOnInit(): void {
+  ngOnInit (): void {
     this.mainSrv
       .getAction('getList', URL.apiURL + URL.CharactersURL)
       .subscribe((res: any) => {
-        this.randomList = res.data.results.sort(() => Math.random() - 0.5);
+        this.randomList = res.data.results.sort(() => Math.random() - 0.5)
         for (let i = 0; i < 4; i++) {
-          this.topHeros.push(this.randomList[i]);
+          this.topHeros.push(this.randomList[i])
         }
-      });
+      })
   }
 
-  goCharacter(characters: any) {
-    this.mainSrv.character = characters;
+  goCharacter (characters: any) {
+    this.mainSrv.character = characters
   }
 }

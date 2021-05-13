381e3f6a3fa37e738d58487c00969ef28a92ce60
Aday <aday69@hotmail.com>
2021-05-13 12:11:31 +0200

    changes in details & list component

--- 00_Projects/RAM/RAM/src/app/list/list.component.spec.ts
+++ 00_Projects/RAM/RAM/src/app/list/list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from './list.component';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -12,7 +13,7 @@ describe('ListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ListComponent ],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       providers: [ListComponent]
     })
     .compileComponents();

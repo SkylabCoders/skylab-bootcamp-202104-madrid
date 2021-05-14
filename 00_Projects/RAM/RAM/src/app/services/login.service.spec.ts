c9f2ab1bcacd30499917110f6a663011136e5c6b
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-14 12:45:56 +0200

    Upload

--- 00_Projects/RAM/RAM/src/app/services/login.service.spec.ts
+++ 00_Projects/RAM/RAM/src/app/services/login.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { LoginService } from './login.service';
 
-let user:Object;
 
 describe('LoginService', () => {
   let service: LoginService;
@@ -14,8 +13,4 @@ describe('LoginService', () => {
    it('should be created', () => {
      expect(service).toBeTruthy();
    });
-   it('should be {name:"Carlos", password:"siiiiiiu"}', ()=>{
-     user = {name:"Carlos", password:"siiiiiiu"};
-     expect(user).toEqual({name:"Carlos", password:"siiiiiiu"})
-   })
  });

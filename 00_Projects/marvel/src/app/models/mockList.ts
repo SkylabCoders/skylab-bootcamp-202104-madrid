2c4d098ada2c998ef5c10e5dc250f0dbc5d4d2e5
Adriancit0 <adrianruanol@gmail.com>
2021-05-15 18:05:20 +0200

    Fixed bug

--- 00_Projects/marvel/src/app/models/mockList.ts
+++ 00_Projects/marvel/src/app/models/mockList.ts
@@ -1,6 +1,6 @@
 import { Imarvel } from './Imarvel'
 
- const marvel:Imarvel [] = [
+const marvel:Imarvel [] = [
   {
     name: 'A/Bomb',
     description: "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! ",

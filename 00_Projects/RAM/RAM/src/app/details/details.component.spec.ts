47d03893de0cd6bcbfe77d83a24949f7f92ee210
Carlos Javier Lopez Gonzalez <carlos@MacBook-Air-de-Carlos.local>
2021-05-16 14:45:25 +0200

    Update last version of details tests

--- 00_Projects/RAM/RAM/src/app/details/details.component.spec.ts
+++ 00_Projects/RAM/RAM/src/app/details/details.component.spec.ts
@@ -3,31 +3,13 @@ import { DetailsComponent } from './details.component';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClient } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing'
-import { Observable, of } from 'rxjs';
-import { Router } from '@angular/router';
 
-
-const CHARACTER_ARRAY= [{gender: 'male',name:'rick', episode: [0, 1, 2]}, {gender: 'female',name:'beth', episode: [0, 1, 2]}, {gender: 'male',name:'morty', episode: [0, 1, 2]}, {gender: 'female',name:'summer', episode: [0, 1, 2]}]
-const prueba = {gender: 'male',name:'rick', episode: [0, 1, 2]};
-let ram:any;
-let imageRam: any[];
-let mockRouter = {
-	navigate: jasmine.createSpy('navigate')
-}
-class MockCharacter {
-  public detailsCharacter = CHARACTER_ARRAY;
-
-  public me(): Observable<any> {
-       return  of(CHARACTER_ARRAY); 
-   }
- }
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
   let httpMock: HttpTestingController;
   let httpClient: HttpClient;
-  let srvMain:MockCharacter;
  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -35,15 +17,11 @@ describe('DetailsComponent', () => {
       imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([
         { path: 'details', component: DetailsComponent}
     ])],
-      providers: [DetailsComponent, {
-        provide: MockCharacter, useClass: MockCharacter
-      }, { provide: Router, useValue: mockRouter},{provide: srvMain, useClass: MockCharacter} ]
+      providers: [DetailsComponent]
     })
     .compileComponents();
     httpMock = TestBed.get(HttpTestingController);
     httpClient = TestBed.inject(HttpClient);
-    // srvMain = TestBed.inject(MockCharacter)
-    srvMain = TestBed.get(MockCharacter);
   });
 
   beforeEach(() => {
@@ -52,6 +30,6 @@ describe('DetailsComponent', () => {
     fixture.detectChanges();
   });
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+     expect(component).toBeTruthy();
+   });
 });

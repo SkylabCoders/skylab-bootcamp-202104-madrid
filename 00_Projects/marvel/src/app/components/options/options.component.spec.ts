e75b458a0924cd3b40f359e9f388755e6f88f4b4
Adriancit0 <adrianruanol@gmail.com>
2021-05-17 12:50:49 +0200

    Update style

--- 00_Projects/marvel/src/app/components/options/options.component.spec.ts
+++ 00_Projects/marvel/src/app/components/options/options.component.spec.ts
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { OptionsComponent } from './options.component';
-
-describe('OptionsComponent', () => {
-  let component: OptionsComponent;
-  let fixture: ComponentFixture<OptionsComponent>;
-
-  beforeEach(() => {
-    const translateServiceStub = () => ({
-      addLangs: ()  => ({}),
-      setDefaultLang: () => ({}),
-      use: () => ({})
-    });
-=======
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { NO_ERRORS_SCHEMA } from '@angular/core'
 import { TranslateService } from '@ngx-translate/core'
@@ -30,36 +13,12 @@ describe('OptionsComponent', () => {
       setDefaultLang: () => ({}),
       use: () => ({})
     })
->>>>>>> features/project/marvel
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [OptionsComponent],
       providers: [
         { provide: TranslateService, useFactory: translateServiceStub }
       ]
-<<<<<<< HEAD
-    });
-    fixture = TestBed.createComponent(OptionsComponent);
-    component = fixture.componentInstance;
-  });
-
-  it('can load instance', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it(`title has default value`, () => {
-    expect(component.title).toEqual(`Lenguages`);
-  });
-
-  it(`english has default value`, () => {
-    expect(component.english).toEqual(`English`);
-  });
-
-  it(`spanish has default value`, () => {
-    expect(component.spanish).toEqual(`Spanish`);
-  });
-});
-=======
     })
     fixture = TestBed.createComponent(OptionsComponent)
     component = fixture.componentInstance
@@ -81,4 +40,3 @@ describe('OptionsComponent', () => {
     expect(component.spanish).toEqual('Spanish')
   })
 })
->>>>>>> features/project/marvel

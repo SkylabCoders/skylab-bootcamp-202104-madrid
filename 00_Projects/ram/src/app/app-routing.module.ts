343c49827a02f5336c315097babfedc27505c60e
Gilberto Cao <gilbe.cao@gmail.com>
2021-05-20 12:13:56 +0200

    Clean dashboard component

--- 00_Projects/ram/src/app/app-routing.module.ts
+++ 00_Projects/ram/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 const routes: Routes = [
   {
-    path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+    path: '',
+    component: DashboardComponent
   },
   {
     path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
@@ -16,9 +18,14 @@ const routes: Routes = [
   },
   {
     path: 'favorites', loadChildren: () => import('./favorites/favorites.module').then(m => m.FavoritesModule)
-  }, 
+  },
   {
     path: 'loadingList', loadChildren: () => import('./loading-list/loading-list.module').then(m => m.LoadingListModule)
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full'
   }
 ]
 
--- 00_Projects/ram/src/app/dashboard/dashboard.module.ts
+++ 00_Projects/ram/src/app/dashboard/dashboard.module.ts
@@ -1,23 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardRoutingModule } from './dashboard-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { DashboardComponent } from './dashboard.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-// import { ShareModuleModule } from '../share-module/share-module.module';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { DashboardComponent } from './dashboard.component'
 
 @NgModule({
   declarations: [
     DashboardComponent
   ],
   imports: [
-    CommonModule,
-    DashboardRoutingModule, 
-    HttpClientModule,
-    MatGridListModule,
-    // ShareModuleModule 
+    CommonModule
   ]
 })
-export class DashboardModule {
- 
- }
+export class DashboardModule {}

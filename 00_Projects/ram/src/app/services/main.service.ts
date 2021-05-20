cc1d283fc22aa56b2e6ca2ff4c348e93ddc428e2
Gilberto Cao <gilbe.cao@gmail.com>
2021-05-20 12:22:41 +0200

    Clear details

--- 00_Projects/ram/src/app/services/main.service.ts
+++ 00_Projects/ram/src/app/services/main.service.ts
@@ -13,6 +13,7 @@ export class MainService {
   favorites:Array<object> = []
   canAddTofavs = false;
 
+  // eslint-disable-next-line no-useless-constructor
   constructor (public srvHttp:HttpService, public srvLogin: LoginService) { }
 
   getTheAPI (url:string):any {

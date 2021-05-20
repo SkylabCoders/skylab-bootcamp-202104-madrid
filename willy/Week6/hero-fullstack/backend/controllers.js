67f71bf51814fd1ae1a80a87129511a3aeff23ab
Willy <acropolis_arcaica@hotmail.com>
2021-05-20 20:11:34 +0200

    Add delete function in backend

--- willy/Week6/hero-fullstack/backend/controllers.js
+++ willy/Week6/hero-fullstack/backend/controllers.js
@@ -1,13 +1,10 @@
-let heroArr = require('./constantes/mockHero')
+
 const Hero = require('./models/heroModel')
 
 function heroesController() {
    
     const getAllHeroes = (req, res) => {
-      if (req.query.name) {
-        return res.json(heroArr.filter(({name}) => name.toLocaleLowerCase().includes(req.query.name.toLocaleLowerCase())))
-      }
-      return res.json(heroArr);
+   
     };
 
     
@@ -36,11 +33,11 @@ function heroesController() {
             res.json(hero)
          };
 
-    const deleteHeroes = (req, res) => {
+    const deleteHeroes = async (req, res) => {
         const {heroId} = req.params
-        heroArr = heroArr.filter(({id}) => id !== +heroId)
+        await Hero.findByIdAndDelete(heroId)
         res.status(204);
-        res.json()
+        res.json();
     };
     return {
       getAllHeroes,
@@ -52,41 +49,4 @@ function heroesController() {
   }
   module.exports = heroesController();
 
-/*let maxHeroId;
- const getHeroId = () => { 
-    const heroOrdered = heroArr.sort((heroA, heroB) => heroA.id - heroB.id);
-    maxHeroId = heroOrdered[heroOrdered.length - 1].id
-  }
 
-module.exports = {
-    getHeroId: () => {
-         maxHeroId;
-        const heroOrdered = heroArr.sort((heroA, heroB) => heroA.id - heroB.id);
-        maxHeroId = heroOrdered[heroOrdered.length - 1].id
-    },
-     
-getAll: (req, res) => {
-    res.json(heroArr);
-},
-post: (req, res) => {
-    this.maxHeroId += 1
-    const newHero = {...req.body, id: maxHeroId}
-    heroArr.push(newHero)
-    res.send(newHero)
-    
-    
-},
-
-getOneHero: (req, res) => {
-    res.send('get funciona con un heroe')
-},
-
-postOneHero: (req, res) => {
-    res.send('post funciona con un heroe')
-},
-
-deleteOneHero: (req, res) => {
-    res.send('delete funciona con un heroe')
-}
-
-}*/

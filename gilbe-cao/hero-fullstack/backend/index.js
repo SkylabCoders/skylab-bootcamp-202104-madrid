1d691b6ccc1a7563bdf57c28ec74c9d95548014a
Gilberto Cao <gilbe.cao@gmail.com>
2021-05-20 18:08:43 +0200

    Connect to atlas database

--- gilbe-cao/hero-fullstack/backend/index.js
+++ gilbe-cao/hero-fullstack/backend/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(
+  process.env.DDBB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 const server = express();
 

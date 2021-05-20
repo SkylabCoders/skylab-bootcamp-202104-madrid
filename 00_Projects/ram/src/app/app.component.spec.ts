8ffceffa638c551fda3c313688f7c08ca6f91f2b
Gilberto Cao <gilbe.cao@gmail.com>
2021-05-20 11:56:22 +0200

    Clear app component tests

--- 00_Projects/ram/src/app/app.component.spec.ts
+++ 00_Projects/ram/src/app/app.component.spec.ts
@@ -1,15 +1,10 @@
-import { TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
-import { HttpClient } from '@angular/common/http';
-import { TranslateService } from '@ngx-translate/core';
+import { TestBed } from '@angular/core/testing'
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+import { RouterTestingModule } from '@angular/router/testing'
+import { AppComponent } from './app.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('AppComponent', () => {
-  let httpMock: HttpTestingController;
-  let httpClient: HttpClient;
-  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -19,30 +14,13 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-      providers: [
-        AppComponent, TranslateService
-      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    }).compileComponents();
-    httpMock = TestBed.get(HttpTestingController);
-    httpClient = TestBed.inject(HttpClient);
-  });
+    }).compileComponents()
+  })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'RAM'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('RAM');
-  });
-
-  it('should run #ngOnInit()', async () => {
-    component.ngOnInit();
-    expect(component.ngOnInit()).toHaveBeenCalled();
-    
-  });
-});
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+})

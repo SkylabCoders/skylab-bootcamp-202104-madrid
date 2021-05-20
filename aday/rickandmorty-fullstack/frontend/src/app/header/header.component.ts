80444017a7d8c44bcf7e6d5ecdfd1230b65aad6c
Aday <aday69@hotmail.com>
2021-05-20 11:17:51 +0200

    - Create fullstack project and configure - Change name of files week6

---
+++ aday/rickandmorty-fullstack/frontend/src/app/header/header.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core'
+import { MainService } from '../services/main.service'
+import { Router } from '@angular/router'
+import { User } from '../services/mocking/user'
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  dashboardTag = 'Dashboard';
+  listTag = 'List';
+  favoriteTag = 'Favourites';
+  logIn = 'Log In';
+  showLogIn = false;
+  model = new User('', '');
+  searchInput = ''
+  ram:Array<object> = []
+  loged = false;
+
+
+  constructor (public srvMain:MainService, public router:Router) { }
+
+  ngOnInit (): void {
+    let el = document.querySelector('.header__logIn');
+    if(!this.loged){
+      el?.classList.add('no-loged');
+    }
+  }
+
+  searchSubmit(){
+    if(this.router.url === '/list'){
+      this.router.navigate(['/loadingList'])
+    }
+    const obs$ = this.srvMain.getTheAPI("https://rickandmortyapi.com/api/character/?name=" + this.searchInput).subscribe((res:any) => {
+      this.ram = res.results;
+      obs$.unsubscribe();
+      this.srvMain.goToList("https://rickandmortyapi.com/api/character/?name=" + this.searchInput)
+      this.router.navigate(['/list'])
+      this.searchInput = ''
+    }, (err:any) => {
+    })
+  }
+
+  openLogIn(){
+    this.showLogIn = !this.showLogIn
+  }
+  onSubmit () {
+    let el = document.querySelector('.header__logIn');
+    el?.classList.remove('no-loged');
+    if(this.showLogIn){
+      this.logIn = 'Log Out';
+    } else {
+      this.logIn = 'Log In'
+    }
+    localStorage.removeItem('username');
+    this.srvMain.currentUser = this.model;
+    this.showLogIn = false;
+    localStorage.setItem('username', JSON.stringify(this.srvMain.currentUser));
+    this.loged = true;
+    this.srvMain.canAddTofavs = true;
+
+  }
+
+  backHome(){
+    this.showLogIn = false;
+  }
+
+}

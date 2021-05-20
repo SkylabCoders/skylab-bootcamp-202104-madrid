ebab5d4a1f04fdf3d5e5be3eb67cf460dd50c29a
Adriancit0 <adrianruanol@gmail.com>
2021-05-20 17:39:10 +0200

    Implemet backend

--- adrian/Curso/week-6/hero-fullstack/frontend/src/app/app.module.ts
+++ adrian/Curso/week-6/hero-fullstack/frontend/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -22,12 +21,7 @@ import { MessagesComponent } from './messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
 
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+   
   ],
   declarations: [
     AppComponent,
--- adrian/Curso/week-6/hero-fullstack/frontend/src/app/hero.service.ts
+++ adrian/Curso/week-6/hero-fullstack/frontend/src/app/hero.service.ts
@@ -11,7 +11,7 @@ import { MessageService } from './message.service';
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = 'http://localhost:4000/api/heroes';  // URL to web api
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })

cba3421dc3b9a50d95bfbd3ea943d80c7d52e8e0
Adan <chema6822@gmail.com>
2021-05-20 18:18:14 +0200

    Add hero model

---
+++ Adan/TOH-full-stack/backend/models/heroModel.js
@@ -0,0 +1,8 @@
+const { model, Schema } = require('mongoose');
+
+const heroSchema = Schema({
+  id: Number,
+  name: String
+}); // schema la forma que va a tener el objeto que se guarda en la base de datos
+
+module.exports = model('Heroes', heroSchema);

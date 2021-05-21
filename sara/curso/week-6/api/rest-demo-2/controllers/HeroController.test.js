37e40abfa1554c286e63ee61826179ef646ac0d9
Sara Cubero <cuberosara@gmail.com>
2021-05-21 13:32:02 +0200

    update heroController test

--- sara/curso/week-6/api/rest-demo-2/controllers/HeroController.test.js
+++ sara/curso/week-6/api/rest-demo-2/controllers/HeroController.test.js
@@ -1,4 +1,5 @@
 const { getAll } = require('./HeroController');
+const Hero = require('../models/heroModel');
 
 jest.mock('../models/heroModel');
 
@@ -16,14 +17,11 @@ describe('heroController', () => {
         };
         await getAll(req, res);
       });
-      test.only('Then call res.json', () => {
+      test('Then call res.json', () => {
         expect(res.json).toHaveBeenCalled();
       });
-      test('Then call res.json with an array of two objects', () => {
-        expect(true).toBe(true);
-      });
       test('Then call Hero.find', () => {
-        expect(true).toBe(true);
+        expect(Hero.find).toHaveBeenCalled();
       });
     });
   });

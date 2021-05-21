ad2fbc63315eb72c02dd86dd4a25be68acbc5479
Renszo Camacho <varsovia84@gmail.com>
2021-05-21 13:02:43 +0200

    Create a test callback controller - Pass the first test

---
+++ Gio-camacho/week-5/toh-fullstack/backend/controllers/callback.test.js
@@ -0,0 +1,36 @@
+/**
+ * GIVEN
+ * WHEN
+ * AND
+ * OR
+ * THEN...
+ */
+// const callback = require('./callback');
+const { getAllHeroes } = require('./callback');
+
+jest.mock('../models/heroModel');
+
+describe('controller function', () => {
+  describe('Given a getAllHeroes function', () => {
+    describe('When is invoked', () => {
+      let req;
+      let res;
+      beforeEach(async () => {
+        // arrange
+        req = {
+          query: null
+        };
+
+        res = {
+          json: jest.fn()
+        };
+
+        // act
+        await getAllHeroes(req, res);
+      });
+      test.only('Then call res.json once', () => {
+        expect(res.json).toHaveBeenCalled();
+      });
+    });
+  });
+});

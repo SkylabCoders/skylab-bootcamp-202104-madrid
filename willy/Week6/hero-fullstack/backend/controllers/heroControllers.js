a7d8d5e090c81d41ca2da3e57899d9b62ce4052f
Willy <acropolis_arcaica@hotmail.com>
2021-05-21 16:24:21 +0200

    Handle errors in heroControllers.js

--- willy/Week6/hero-fullstack/backend/controllers/heroControllers.js
+++ willy/Week6/hero-fullstack/backend/controllers/heroControllers.js
@@ -9,50 +9,75 @@ function heroesController() {
     const getAllHeroes = async (req, res) => {
     
       const query = {...req.query}
-try {
+  try {
 
-  const heroes = await Hero.find(query)
+      const heroes = await Hero.find(query)
  
-   res.json(heroes)
-} catch (error) {
-  res.status(500);
-  res.send(error)
-}
+    res.json(heroes)
+  } catch (error) {
+    res.status(500);
+    res.send(error)
+  }
       
     };
 
     
 
     const createHeroes = async (req, res) => {
-      const newHero = await Hero.create ({
-        ...req.body, 
-      });
+      try {
+
+        const newHero = await Hero.create ({
+          ...req.body, 
+        });
+
+        res.json(newHero);
+      } catch (error) {
+        res.status(500);
+        res.send(error)
+      }
       
-      res.json(newHero);
     }
 
     const getById = async (req, res) => {
+
+      try{
+
         const {heroId} = req.params;
          const hero = await Hero.findById(heroId)
          res.json(hero);
+      } catch (error) {
+        res.status(500);
+        res.send(error)
+      }
     };
     
     const updateHero = async (req, res) => {
         const {heroId} = req.params;
-        const hero = await Hero.findByIdAndUpdate(heroId,
-           {...req.body}, 
-           {new:true}
-           );
-
-            res.json(hero)
+        try {
+          const hero = await Hero.findByIdAndUpdate(heroId,
+             {...req.body}, 
+             {new:true}
+             );
+  
+              res.json(hero)
+        } catch (error) {
+          res.status(500);
+          res.send(error)
+        }
          };
 
     const deleteHeroes = async (req, res) => {
-        const {heroId} = req.params
+      const {heroId} = req.params
+      
+      try {
         await Hero.findByIdAndDelete(heroId)
-        res.status(204);
+    
         res.json();
+       } catch (error) {
+        res.status(204);
+        res.send(error)
     };
+  }
     return {
       getAllHeroes,
       createHeroes,

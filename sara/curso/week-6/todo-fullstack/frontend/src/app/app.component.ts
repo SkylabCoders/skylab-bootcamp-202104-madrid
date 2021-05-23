14c62aa933cda9355bbe3d082e13da71686942ad
Sara Cubero <cuberosara@gmail.com>
2021-05-23 15:34:09 +0200

    Create list component Set up update option Set up delete option Set up add option

--- sara/curso/week-6/todo-fullstack/frontend/src/app/app.component.ts
+++ sara/curso/week-6/todo-fullstack/frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import Task from './models/task';
-import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
@@ -8,48 +6,6 @@ import { TaskService } from './services/task.service';
   styleUrls: ['./app.component.scss'],
 })
 // eslint-disable-next-line import/prefer-default-export
-export class AppComponent implements OnInit {
-  tasks:any
-
-  wantsToUpdate = false;
-
-  constructor(private taskService:TaskService) {}
-
-  ngOnInit() {
-    this.paintList();
-  }
-
-  paintList() {
-    this.taskService.getAll().subscribe((res:any) => {
-      this.tasks = res;
-    });
-  }
-
-  async addNewTask(newTask:string) {
-    this.taskService.create(newTask).subscribe();
-    setTimeout(() => {
-      this.paintList();
-    }, 500);
-  }
-
-  deleteTask(id:string) {
-    this.taskService.delete(id).subscribe();
-    setTimeout(() => {
-      this.paintList();
-    }, 500);
-  }
-
-  goToUpdate() {
-    this.wantsToUpdate = true;
-  }
-
-  update(id:string, updateTask:string) {
-    console.log(id);
-    console.log(updateTask);
-    this.wantsToUpdate = false;
-    this.taskService.update(id, updateTask).subscribe();
-    setTimeout(() => {
-      this.paintList();
-    }, 500);
-  }
+export class AppComponent {
+  constructor() {}
 }
--- sara/curso/week-6/todo-fullstack/frontend/src/app/app.module.ts
+++ sara/curso/week-6/todo-fullstack/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 // eslint-disable-next-line import/no-unresolved
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,

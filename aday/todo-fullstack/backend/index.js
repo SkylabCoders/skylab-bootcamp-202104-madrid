bb3a59643b195a4f97501fa6356f96cf98c17b6a
Aday <aday69@hotmail.com>
2021-05-23 13:23:40 +0200

    Setup index.js backend

--- aday/todo-fullstack/backend/index.js
+++ aday/todo-fullstack/backend/index.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const cors = require('cors');
 const chalk = require('chalk');
+const cors = require('cors');
 const debug = require('debug')('app');
 require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-const { connect } = require('mongoose');
-
-connect(
-  process.env.DDB_URL,
+mongoose.connect(process.env.DDBB_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
-);
+  });
 
 const app = express();
 
-const tasksRoutes = require('./routes/tasksRoutes');
-
-const port = process.env.PORT || 4000;
-
 app.use(cors());
-
+app.use(morgan('dev'));
 app.use(express.json());
 
+const tasksRoutes = require('./routes/tasksRoutes');
+
 app.use('/api/tasks', tasksRoutes);
+
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => debug(`Server is running in ${chalk.blue(`http://localhost:${port}`)}`));

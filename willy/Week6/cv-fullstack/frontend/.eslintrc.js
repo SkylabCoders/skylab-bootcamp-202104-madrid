f1839cb4c994cb06e0a244c78cf87bee8f871024
Willy <acropolis_arcaica@hotmail.com>
2021-05-24 16:36:08 +0200

    Install and configure eslint

---
+++ willy/Week6/cv-fullstack/frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows'],
+    'comma-dangle': [2, 'never']
+  }
+};

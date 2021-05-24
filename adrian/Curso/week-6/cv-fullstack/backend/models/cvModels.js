1e69cf2e5e53dc87bbc081585caaf0535ce7e01a
Adriancit0 <adrianruanol@gmail.com>
2021-05-24 12:32:36 +0200

    Create model

---
+++ adrian/Curso/week-6/cv-fullstack/backend/models/cvModels.js
@@ -0,0 +1,14 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = Schema({
+  skills: Array,
+  experience: Array,
+  education: Array,
+  userInfo: {
+    name: String,
+    address: String,
+    phone: Number
+  }
+});
+
+module.exports = model('User', userSchema);

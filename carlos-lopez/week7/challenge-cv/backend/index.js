0ca8da799e47bba206ed3782272df51b07a21547
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-24 12:21:48 +0200

    Connect mongoose

--- carlos-lopez/week7/challenge-cv/backend/index.js
+++ carlos-lopez/week7/challenge-cv/backend/index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(
+  process.env.DDBB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);

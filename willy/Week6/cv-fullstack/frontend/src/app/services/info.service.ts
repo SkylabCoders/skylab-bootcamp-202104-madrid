23583f6707e541fb84de28926d8220c6de0170e5
Willy <acropolis_arcaica@hotmail.com>
2021-05-24 18:14:29 +0200

    Update service

--- willy/Week6/cv-fullstack/frontend/src/app/services/info.service.ts
+++ willy/Week6/cv-fullstack/frontend/src/app/services/info.service.ts
@@ -1,18 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import Info from '../models/info';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment'
+import Info from '../models/info'
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoService {
-  constructor(private httpSvr: HttpClient) { }
+  constructor (private httpSvr: HttpClient) { }
 
-  getInfo(): Observable<Info[]> {
-    this.httpSvr.get <Info[]>(
-      environment.infoApiUrl
-    );
+  getData (url:string):any {
+    return this.httpSvr.get(url)
+  }
+
+  addData (url:string, body:string):any {
+    return this.httpSvr.post(url, body)
+  }
+
+  updateData (url:string, body:string):any {
+    return this.httpSvr.put(url, body)
+  }
+
+  deleteData (url:string):any {
+    return this.httpSvr.delete(url)
   }
 }

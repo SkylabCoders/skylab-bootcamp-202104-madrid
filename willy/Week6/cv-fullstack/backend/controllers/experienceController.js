1e0600dedc0365bb1108853fa69c414ea11b65c7
Willy <acropolis_arcaica@hotmail.com>
2021-05-24 15:38:50 +0200

    Add experience controller and route

---
+++ willy/Week6/cv-fullstack/backend/controllers/experienceController.js
@@ -0,0 +1,54 @@
+const Experience = require('../model/experience');
+
+function controllerInfo() {
+  async function getAll(req, res) {
+    try {
+      const experience = await Experience.find(req.query);
+      res.json(experience);
+    } catch (error) {
+      res.status(500);
+      res.send(error);
+    }
+  }
+
+  async function create(req, res) {
+    try {
+      const experience = await Experience.create(req.body);
+      res.json(experience);
+    } catch (error) {
+      res.status(500);
+      res.send(error);
+    }
+  }
+
+  async function updateById(req, res) {
+    try {
+      const experience = await Experience.findByIdAndUpdate(
+        req.params.expId,
+        req.body,
+        { new: true }
+      );
+      res.json(experience);
+    } catch (error) {
+      res.status(500);
+      res.send(error);
+    }
+  }
+
+  async function deleteById(req, res) {
+    try {
+      await Experience.findByIdAndDelete(req.params.expId);
+      res.status(204);
+      res.send();
+    } catch (error) {
+      res.status(500);
+      res.send(error);
+    }
+  }
+
+  return {
+    getAll, create, updateById, deleteById
+  };
+}
+
+module.exports = controllerInfo();

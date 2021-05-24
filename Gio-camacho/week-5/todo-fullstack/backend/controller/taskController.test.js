7da9c444e9415c6c356357c636ea7c56ee985623
Renszo Camacho <varsovia84@gmail.com>
2021-05-24 07:26:00 +0200

    Test in taskController - Test deleteById function

--- Gio-camacho/week-5/todo-fullstack/backend/controller/taskController.test.js
+++ Gio-camacho/week-5/todo-fullstack/backend/controller/taskController.test.js
@@ -178,4 +178,59 @@ describe('taskController', () => {
       });
     });
   });
+
+  describe('Given a \'deleteById\' function', () => {
+    describe('When is invoked', () => {
+      let req;
+      let res;
+
+      describe('And there is no errors', () => {
+        beforeEach(async () => {
+          req = {
+            params: { taskId: null }
+          };
+
+          res = {
+            status: jest.fn(),
+            send: jest.fn()
+          };
+
+          await taskController.deleteById(req, res);
+        });
+
+        test('Then call res.send once', () => {
+          expect(res.send).toHaveBeenCalled();
+        });
+
+        test('Then call \'taskModel.findByIdAndDelete\'', () => {
+          expect(taskModel.findByIdAndDelete).toHaveBeenCalled();
+        });
+      });
+
+      describe('And there is errors', () => {
+        beforeEach(async () => {
+          req = {
+            params: { taskId: null }
+          };
+
+          res = {
+            status: jest.fn(),
+            send: jest.fn()
+          };
+
+          taskModel.findByIdAndDelete.mockRejectedValueOnce('findByIdAndDelete error');
+
+          await taskController.deleteById(req, res);
+        });
+
+        test('Then call res.status with 500', () => {
+          expect(res.status).toHaveBeenCalledWith(500);
+        });
+
+        test('Then call \'findByIdAndDelete error\'', () => {
+          expect(res.send).toHaveBeenCalledWith('findByIdAndDelete error');
+        });
+      });
+    });
+  });
 });

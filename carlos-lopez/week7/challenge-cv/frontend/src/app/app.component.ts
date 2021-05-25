4994e6b660892925c9a8c8ba9560238579ec291c
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-25 12:29:57 +0200

    Change variables for being more semantic

--- carlos-lopez/week7/challenge-cv/frontend/src/app/app.component.ts
+++ carlos-lopez/week7/challenge-cv/frontend/src/app/app.component.ts
@@ -35,55 +35,55 @@ export class AppComponent implements OnInit {
   }
 
   addExperience(
-    id:string, knowledgeArr:any, valueExp:string, valueYear: any, valueAcademy:any,
+    id:string, knowledgeArray:any, valueExp:string, valueYear: any, valueAcademy:any,
   ) {
-    const myArr = knowledgeArr;
-    myArr.push({
+    const newKnowledgeArray = knowledgeArray;
+    newKnowledgeArray.push({
       language: valueExp,
       academy: valueAcademy,
       year: valueYear,
     });
-    this.userService.addExperience(id, myArr)
+    this.userService.addExperience(id, newKnowledgeArray)
       .pipe(
         tap(() => this.refresh$.next()),
       )
       .subscribe();
   }
 
-  delete(id: string, knowledgeArr:any, i:any) {
-    const myArr = knowledgeArr;
-    myArr.splice(i, 1);
-    this.userService.deleteExperience(id, myArr)
+  delete(id: string, knowledgeArray:any, i:any) {
+    const newKnowledgeArray = knowledgeArray;
+    newKnowledgeArray.splice(i, 1);
+    this.userService.deleteExperience(id, newKnowledgeArray)
       .pipe(
         tap(() => this.refresh$.next()),
       )
       .subscribe();
   }
 
-  updateLanguage(id: string, knowledgeArr:any, i:any, skills: string) {
-    const myArr = knowledgeArr;
-    myArr[i].language = skills;
-    this.userService.updateExperience(id, myArr)
+  updateLanguage(id: string, knowledgeArray:any, i:any, skills: string) {
+    const newKnowledgeArray = knowledgeArray;
+    newKnowledgeArray[i].language = skills;
+    this.userService.updateExperience(id, newKnowledgeArray)
       .pipe(
         tap(() => this.refresh$.next()),
       )
       .subscribe();
   }
 
-  updateAcademy(id: string, knowledgeArr:any, i:any, skills: any) {
-    const myArr = knowledgeArr;
-    myArr[i].academy = skills;
-    this.userService.updateExperience(id, myArr)
+  updateAcademy(id: string, knowledgeArray:any, i:any, skills: any) {
+    const newKnowledgeArray = knowledgeArray;
+    newKnowledgeArray[i].academy = skills;
+    this.userService.updateExperience(id, newKnowledgeArray)
       .pipe(
         tap(() => this.refresh$.next()),
       )
       .subscribe();
   }
 
-  updateYear(id: string, knowledgeArr:any, i:any, skills: any) {
-    const myArr = knowledgeArr;
-    myArr[i].year = skills;
-    this.userService.updateExperience(id, myArr)
+  updateYear(id: string, knowledgeArray:any, i:any, skills: any) {
+    const newKnowledgeArray = knowledgeArray;
+    newKnowledgeArray[i].year = skills;
+    this.userService.updateExperience(id, newKnowledgeArray)
       .pipe(
         tap(() => this.refresh$.next()),
       )
--- carlos-lopez/week7/challenge-cv/frontend/src/app/app.module.ts
+++ carlos-lopez/week7/challenge-cv/frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-// import { FuncionalityComponent } from './components/funcionality/funcionality.component';
 
 @NgModule({
   declarations: [

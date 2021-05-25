b6ed95770ffb07bce59960e7936eb6c47b6b6d12
Renszo Camacho <varsovia84@gmail.com>
2021-05-25 09:34:46 +0200

    Generate cv component - Generate aside component - Generate main component

--- Gio-camacho/week-5/resume-fullstack/frontend/src/app/app.module.ts
+++ Gio-camacho/week-5/resume-fullstack/frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CvComponent } from './component/cv/cv.component';
+import { AsideComponent } from './component/cv/aside/aside.component';
+import { MainComponent } from './component/cv/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    AsideComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule

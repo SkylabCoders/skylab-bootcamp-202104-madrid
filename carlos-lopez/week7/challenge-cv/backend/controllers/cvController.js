f5b142e8305978d017173bac647d974be42cc223
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-25 12:06:41 +0200

    Delete debug()

--- carlos-lopez/week7/challenge-cv/backend/controllers/cvController.js
+++ carlos-lopez/week7/challenge-cv/backend/controllers/cvController.js
@@ -1,13 +1,10 @@
-const debug = require('debug')('app:cvController');
 const Cv = require('../models/cvModel');
 
 function controller() {
   const getAll = async (req, res) => {
-    debug(req.query);
     const query = { ...req.query };
     try {
       const cv = await Cv.find(query);
-      debug(cv);
       res.json(cv);
     } catch (error) {
       res.status(500);

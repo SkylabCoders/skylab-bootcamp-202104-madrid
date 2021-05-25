eed0fc10f228ef78909773f8db4e11faa001ba2d
Aday <aday69@hotmail.com>
2021-05-25 16:50:38 +0200

    Configure environment Create models interface

--- aday/Challenges-Course/week7/realstate/src/environments/environment.ts
+++ aday/Challenges-Course/week7/realstate/src/environments/environment.ts
@@ -1,16 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
-};
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  production: true,
+  urlApi: ' https://apidev.holapisos.com/es/api/node/inmuebles?page[limit]=32&page[off set]=0&sort[weight][path]=field_inmu_peso_orde&sort[weight][direction]=DES C&sort[relevancy][path]=relevancy&sort[relevancy][direction]=DESC&fields[no de-- inmuebles]=field_inmu_imag_arra,field_inmu_prec,field_inmu_nume_habi,field_in mu_refe,field_inmu_pobl,field_inmu_area_cons,field_inmu_tipo_sin_agru,field_inm u_tipo_via,field_inmu_nomb_call'
+}

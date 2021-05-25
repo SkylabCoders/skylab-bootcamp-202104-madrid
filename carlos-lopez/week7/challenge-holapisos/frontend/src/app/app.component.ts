fa96d99393b95559e9656ca8b3515aa30d65b6d2
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-25 16:15:33 +0200

    Build bundle

--- carlos-lopez/week7/challenge-holapisos/frontend/src/app/app.component.ts
+++ carlos-lopez/week7/challenge-holapisos/frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+/* eslint-disable no-useless-constructor */
+import { Component } from '@angular/core'
+import { HttpService } from './services/http.service'
+import { OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor (public httpService: HttpService) {}
   title = 'frontend';
+  ngOnInit () {
+    this.httpService.getTheApi()
+  }
 }
--- carlos-lopez/week7/challenge-holapisos/frontend/src/app/services/http.service.ts
+++ carlos-lopez/week7/challenge-holapisos/frontend/src/app/services/http.service.ts
@@ -1,9 +1,17 @@
+/* eslint-disable no-useless-constructor */
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { environment } from '../../environments/environment'
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  url = environment.realEstateApiUrl;
   constructor (private httpClient: HttpClient) { }
+
+  getTheApi ():Observable<Object> {
+    return this.httpClient.get(this.url)
+  }
 }

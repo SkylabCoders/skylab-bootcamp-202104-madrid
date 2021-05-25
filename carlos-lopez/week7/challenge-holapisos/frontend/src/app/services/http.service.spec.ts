76632ff562b720ed8b2123449a5d7ad1f387f05d
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-25 19:18:27 +0200

    Set testing files

--- carlos-lopez/week7/challenge-holapisos/frontend/src/app/services/http.service.spec.ts
+++ carlos-lopez/week7/challenge-holapisos/frontend/src/app/services/http.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing'
 
-import { HttpService } from './http.service';
+import { HttpService } from './http.service'
 
 describe('HttpService', () => {
-  let service: HttpService;
+  let service: HttpService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
-  });
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(HttpService)
+  })
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+})

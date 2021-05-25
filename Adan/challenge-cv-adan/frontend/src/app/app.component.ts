0e744753e68fd95696403e0667bbcaed31a40ad5
Adan <chema6822@gmail.com>
2021-05-25 10:45:34 +0200

    Add fn to ng on init

--- Adan/challenge-cv-adan/frontend/src/app/app.component.ts
+++ Adan/challenge-cv-adan/frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core'
 import { Observable, Subject } from 'rxjs'
 import { CvService } from './service/cv.service'
+import { switchMap, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -17,10 +18,22 @@ export class AppComponent implements OnInit, AfterViewInit {
   constructor (public cvService: CvService) {}
 
   ngOnInit () {
-
+    this.fetchCv$ = this.refresh$
+      .pipe(
+        switchMap(() => this.cvService.fetchCv())
+      )
   }
 
   ngAfterViewInit () {
-
+    this.refresh$.next()
   }
+
+  addCv (value:string) {
+    this.cvService.postCv(value)
+      .pipe(
+        tap(() => this.refresh$.next())
+      ).subscribe()
+  }
+
+  // updateExperience(id:string,)
 }

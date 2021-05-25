bb60ab9bae1264464f09335a7c85e37eba020dc4
Willy <acropolis_arcaica@hotmail.com>
2021-05-25 20:15:59 +0200

    Import features in app.module
    -httpClientModule

--- willy/week7/pisos/src/app/app.module.ts
+++ willy/week7/pisos/src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component'
+import { CardsComponent } from './cards/cards.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

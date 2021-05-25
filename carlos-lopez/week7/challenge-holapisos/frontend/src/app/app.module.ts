c67b1dd3ca59342c3ded38d1c291373e9726b650
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-25 15:44:00 +0200

    Set service http

--- carlos-lopez/week7/challenge-holapisos/frontend/src/app/app.module.ts
+++ carlos-lopez/week7/challenge-holapisos/frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
+import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

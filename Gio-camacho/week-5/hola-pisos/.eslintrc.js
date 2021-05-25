90e84d44d2ddaa2d1912327f3d3959edd9386f4e
Renszo Camacho <varsovia84@gmail.com>
2021-05-25 15:21:28 +0200

    Install eslint

---
+++ Gio-camacho/week-5/hola-pisos/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'comma-dangle': [2, 'never']
+  }
+}

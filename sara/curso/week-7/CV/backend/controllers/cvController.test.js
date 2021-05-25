e94b5251451969fff0e43aa55ebec5f0a5da4589
Sara Cubero <cuberosara@gmail.com>
2021-05-25 18:35:24 +0200

    Create cvController test add getAll function test add getUserData funtion test

--- sara/curso/week-7/CV/backend/controllers/cvController.test.js
+++ sara/curso/week-7/CV/backend/controllers/cvController.test.js
@@ -8,3 +8,96 @@ const {
 const User = require('../models/cvModel');
 
 jest.mock('../models/cvModel');
+
+describe('cvController', () => {
+  describe('Given a getAll function', () => {
+    describe('When is invoked', () => {
+      let req;
+      let res;
+      describe('And there is no error', () => {
+        beforeEach(async () => {
+          req = {
+            query: {},
+          };
+          res = {
+            json: jest.fn(),
+          };
+          await getAll(req, res);
+        });
+        test('Then call res.json', () => {
+          expect(res.json).toHaveBeenCalled();
+        });
+        test('Then call User.find', () => {
+          expect(User.find).toHaveBeenCalled();
+        });
+      });
+      describe('And there is an error', () => {
+        beforeEach(async () => {
+          req = {
+            query: {},
+          };
+          res = {
+            json: jest.fn(),
+            send: jest.fn(),
+            status: jest.fn(),
+          };
+
+          User.find.mockRejectedValueOnce('getAll error');
+
+          await getAll(req, res);
+        });
+        test('Then call status with 500', () => {
+          expect(res.status).toHaveBeenCalledWith(500);
+        });
+        test('Then call res.send with getAll error', () => {
+          expect(res.send).toHaveBeenCalledWith('getAll error');
+        });
+      });
+    });
+  });
+  describe('Given a getUserData function', () => {
+    describe('When is invoked', () => {
+      let req;
+      let res;
+      describe('And there is no error', () => {
+        beforeEach(async () => {
+          req = {
+            params: { userId: 'myId' },
+          };
+          res = {
+            json: jest.fn(),
+          };
+          await getUserData(req, res);
+        });
+        test('Then call res.json', () => {
+          expect(res.json).toHaveBeenCalled();
+        });
+        test('Then call User.find', () => {
+          expect(User.findById).toHaveBeenCalled();
+        });
+      });
+      describe('And there is an error', () => {
+        beforeEach(async () => {
+          req = {
+            params: { userId: 'myId' },
+          };
+          res = {
+            json: jest.fn(),
+            send: jest.fn(),
+            status: jest.fn(),
+          };
+
+          User.findById.mockRejectedValueOnce('getUserData error');
+
+          await getUserData(req, res);
+        });
+        test('Then call status with 500', () => {
+          expect(res.status).toHaveBeenCalledWith(500);
+        });
+        test('Then call res.send with getUserData error', () => {
+          expect(res.send).toHaveBeenCalledWith('getUserData error');
+        });
+      });
+    });
+  });
+});

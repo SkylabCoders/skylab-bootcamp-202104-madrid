79af48079a43611d07a7db8273bd27fd105b1b90
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-26 18:38:49 +0200

    Set up App component works

--- carlos-lopez/week7/challenge-cv/frontend/src/app/app.component.spec.ts
+++ carlos-lopez/week7/challenge-cv/frontend/src/app/app.component.spec.ts
@@ -1,21 +1,29 @@
-// import { TestBed } from '@angular/core/testing'
-// import { AppComponent } from './app.component'
+import { TestBed, ComponentFixture } from '@angular/core/testing'
+import { AppComponent } from './app.component'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { of } from 'rxjs'
+import { UserService } from 'src/app/services/user.service'
+describe('AppComponent', () => {
+  let component: AppComponent
+  let fixture: ComponentFixture<AppComponent>
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [UserService]
+    }).compileComponents()
+  })
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [
-//         AppComponent
-//       ]
-//     }).compileComponents()
-//   })
-//   beforeEach(() => {
-
-//   })
-
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent)
-//     const app = fixture.componentInstance
-//     expect(app).toBeTruthy()
-//   })
-// })
+  it('should create the app component', () => {
+    expect(component).toBeTruthy()
+  })
+})

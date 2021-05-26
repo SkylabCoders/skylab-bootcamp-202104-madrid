27c8acd5f47801cbf0bcb3df2ca3765284a6e6e6
Adan <chema6822@gmail.com>
2021-05-26 12:41:32 +0200

    Add testing in service -Add testing in app component

--- Adan/holaPisos/frontend/src/app/app.component.spec.ts
+++ Adan/holaPisos/frontend/src/app/app.component.spec.ts
@@ -1,31 +1,39 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { Component } from '@angular/core'
+import { TestBed } from '@angular/core/testing'
+import { AppComponent } from './app.component'
+import { HttpService } from './service/http.service'
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
+  let comp: Component
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      // provide the component-under-test and dependent service
+      providers: [
+        AppComponent,
+        { provide: HttpService }
+      ]
+    })
+    // inject both the component and the dependent service.
+    comp = TestBed.inject(AppComponent)
+    httpService = TestBed.inject(HttpService)
+  })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
 
-  it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
-  });
+  it('should have as title \'frontend\'', () => {
+    const fixture = TestBed.createComponent(AppComponent)
+    const app = fixture.componentInstance
+    expect(app.title).toEqual('frontend')
+  })
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('frontend app is running!');
-  });
-});
+    const fixture = TestBed.createComponent(AppComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement
+    expect(compiled.querySelector('.content span').textContent).toContain('frontend app is running!')
+  })
+})
--- Adan/holaPisos/frontend/src/app/service/http.service.spec.ts
+++ Adan/holaPisos/frontend/src/app/service/http.service.spec.ts
@@ -1,16 +1,35 @@
-import { TestBed } from '@angular/core/testing';
-
-import { HttpService } from './http.service';
+import { HttpService } from './http.service'
+import { HttpClient } from '@angular/common/http'
+import { TestBed } from '@angular/core/testing'
+import { of } from 'rxjs'
 
 describe('HttpService', () => {
-  let service: HttpService;
+  let service: HttpService
+  let httpClientSpy: {get: jasmine.Spy}
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
-  });
-
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: httpClientSpy }]
+    })
+    service = TestBed.inject(HttpService)
+  })
   it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    expect(service).toBeTruthy()
+  })
+  it('getUser should be called', (done) => {
+    httpClientSpy.get.and.returnValue(of({}))
+    service.getApi().subscribe(() => {
+      expect(httpClientSpy.get.calls.count()).toBe(1)
+      done()
+    })
+  })
+  it('getUser should be called', (done) => {
+    const url:string = 'httploquesea'
+    httpClientSpy.get.and.returnValue(of({}))
+    service.getApiMostrar(url).subscribe(() => {
+      expect(httpClientSpy.get.calls.count()).toBe(1)
+      done()
+    })
+  })
+})

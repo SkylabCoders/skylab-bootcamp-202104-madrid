860f7f667ee6e6ded9bdbb8a67b5a0507131a31b
Adan <chema6822@gmail.com>
2021-05-26 17:51:16 +0200

    Add karma config to package json -Add jasmin config to eslint -Add extra funcionality to user.service to test

--- Adan/challenge-cv-adan/frontend/src/environments/environment.ts
+++ Adan/challenge-cv-adan/frontend/src/environments/environment.ts
@@ -5,8 +5,8 @@
 // eslint-disable-next-line import/prefer-default-export
 export const environment = {
   production: false,
-  cvApiUrl: 'http://localhost:4000/api/cv',
-};
+  cvApiUrl: 'http://localhost:4000/api/cv'
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.

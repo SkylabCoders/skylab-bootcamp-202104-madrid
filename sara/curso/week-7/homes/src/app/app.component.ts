fb8b3dee5d204e1a234d4a6211aa1167812634c7
Sara Cubero <cuberosara@gmail.com>
2021-05-26 16:39:04 +0200

    Implement pipe comma-to-dot

--- sara/curso/week-7/homes/src/app/app.component.ts
+++ sara/curso/week-7/homes/src/app/app.component.ts
@@ -7,12 +7,12 @@ import { HttpService } from './services/http.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent implements OnInit {
-  url = 'https://apidev.holapisos.com/es/api/node/inmuebles?page%5Boffset%5D=0&page%5Blimit%5D=32&sort%5Bweight%5D%5Bpath%5D=field_inmu_peso_orde&sort%5Bweight%5D%5Bdirection%5D=DESC&sort%5Brelevancy%5D%5Bpath%5D=relevancy&sort%5Brelevancy%5D%5Bdirection%5D=DESC&fields%5Bnode--inmuebles%5D=field_inmu_imag_arra%2Cfield_inmu_prec%2Cfield_inmu_nume_habi%2Cfield_inmu_refe%2Cfield_inmu_pobl%2Cfield_inmu_area_cons%2Cfield_inmu_tipo_sin_agru%2Cfield_inmu_tipo_via%2Cfield_inmu_nomb_call'
 
-  houses:any
-
-  housesForPagination:any
+  url = `https://apidev.holapisos.com/es/api/node/inmuebles?page%5Boffset%5D=0&page%5Blimit%5D=32&sort%5Bweight%5D%5Bpath%5D=field_inmu_peso_orde&sort%5Bweight%5D%5Bdirection%5D=DESC&sort%5Brelevancy%5D%5Bpath%5D=relevancy&sort%5Brelevancy%5D%5Bdirection%5D=DESC&fields%5Bnode--inmuebles%5D=field_inmu_imag_arra%2Cfield_inmu_prec%2Cfield_inmu_nume_habi%2Cfield_inmu_refe%2Cfield_inmu_pobl%2Cfield_inmu_area_cons%2Cfield_inmu_tipo_sin_agru%2Cfield_inmu_tipo_via%2Cfield_inmu_nomb_call`
+  houses: Array<any> = []
+  housesForPagination:any;
 
   constructor(private httpService:HttpService) {}
 
@@ -22,22 +22,18 @@ export class AppComponent implements OnInit {
 
   paint(url:string) {
     this.httpService.getAll(url).subscribe((res:any) => {
-      this.houses = res.data;
       this.housesForPagination = res;
+      res.data.forEach((element:any) => {
+        this.houses.push(element)
+      });
+      console.log(typeof this.houses[0].attributes.field_inmu_prec)
+      console.log(res.data)
     });
   }
 
-  goToPrev() {
-    if (this.housesForPagination.links.prev) {
-      this.url = this.housesForPagination.links.prev.href;
-      this.paint(this.url);
-    }
-  }
-
-  goToNext() {
-    if (this.housesForPagination.links.next) {
+  loadMore() {
       this.url = this.housesForPagination.links.next.href;
       this.paint(this.url);
-    }
+    
   }
 }
--- sara/curso/week-7/homes/src/app/app.module.ts
+++ sara/curso/week-7/homes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { CommaToDotPipe } from './pipes/comma-to-dot.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
+    CommaToDotPipe
   ],
   imports: [
     BrowserModule,

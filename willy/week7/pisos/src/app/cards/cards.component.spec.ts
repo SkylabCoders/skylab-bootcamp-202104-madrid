34b50163b00e2845ff7bba109cdecc2d9c3fccf6
Willy <acropolis_arcaica@hotmail.com>
2021-05-26 12:30:53 +0200

    Add test cards.component

--- willy/week7/pisos/src/app/cards/cards.component.spec.ts
+++ willy/week7/pisos/src/app/cards/cards.component.spec.ts
@@ -1,25 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CardsComponent } from './cards.component';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing'
+import { HttpService } from '../services/http.service'
+import { CardsComponent } from './cards.component'
 
 describe('CardsComponent', () => {
-  let component: CardsComponent;
-  let fixture: ComponentFixture<CardsComponent>;
+  let component: CardsComponent
+  let fixture: ComponentFixture<CardsComponent>
+  let service: HttpService
+  let spy:any
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardsComponent ]
+      declarations: [CardsComponent]
     })
-    .compileComponents();
-  });
+      .compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CardsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(CardsComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+
+  it('should call ngOnInit using http.service', () => {
+    fixture.componentInstance.ngOnInit()
+    tick()
+    fixture.detectChanges()
+    expect(component.ngOnInit).toHaveBeenCalled()
+  }
+  )
+})

097a59c7c9d09fd1f15a732270b874a8c98fd4ad
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-26 16:14:18 +0200

    Test 100%

--- carlos-lopez/week7/challenge-holapisos/frontend/src/app/app.component.spec.ts
+++ carlos-lopez/week7/challenge-holapisos/frontend/src/app/app.component.spec.ts
@@ -1,54 +1,64 @@
-import { TestBed } from '@angular/core/testing'
+import { TestBed, ComponentFixture } from '@angular/core/testing'
 import { AppComponent } from './app.component'
 import { HttpService } from './services/http.service'
-import { HttpClient } from '@angular/common/http'
-import { defer, Observable } from 'rxjs'
+import { of } from 'rxjs'
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
 describe('AppComponent', () => {
-  let service: HttpService
-  let httpClientSpy: { get: jasmine.Spy }
-
+  let component: AppComponent
+  let fixture: ComponentFixture<AppComponent>
   beforeEach(async () => {
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
-    service = new HttpService(httpClientSpy as any)
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [HttpClientTestingModule],
       providers: [
-        AppComponent,
-        { provide: HttpClient, useValue: httpClientSpy }
+        HttpService
       ]
     }).compileComponents()
-    service = TestBed.inject(HttpService)
   })
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app).toBeTruthy()
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
   })
-
-  //   it('should return expected heroes (HttpClient called once)', (done) => {
-  //     const fixture = TestBed.createComponent(AppComponent)
-  //     const app = fixture.componentInstance
-  //     app.chargePage()
-  //     service.getTheApi()
-  //     expect(app.chargePage).toHaveBeenCalled()
-  //   })
-  it('should return expected heroes (HttpClient called once)', (done: DoneFn) => {
-    const expectedHeroes =
-      [{ id: 1, name: 'A' }, { id: 2, name: 'B' }]
-    httpClientSpy.get.and.returnValue(function asyncData<T> (expectedHeroes:any) {
-      return defer(() => Promise.resolve(expectedHeroes))
-    })
-    service.getTheApi().subscribe(
-      heroes => {
-        expect(heroes).toEqual(expectedHeroes, 'expected heroes')
-        done()
-      },
-      done.fail
-    )
-    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call')
+  it('should create', () => {
+    expect(component).toBeTruthy()
+  })
+  it('#chargePage should be called', () => {
+    const fetchFlat$ = {
+      info: 'string',
+      street: 'string',
+      price: 'string',
+      img: 'string'
+    }
+    const spyFn = spyOn(component.httpService, 'getTheApi').and.returnValue(of(fetchFlat$))
+    component.chargePage()
+    expect(spyFn).toHaveBeenCalled()
+  })
+  it('getNextPage should be called', () => {
+    const fetchFlat$ = {
+      info: 'string',
+      street: 'string',
+      price: 'string',
+      img: 'string'
+    }
+    component.nextUrl = 'ghh'
+    const spyFn = spyOn(component.httpService, 'getTheApiNext').and.returnValue(of(fetchFlat$))
+    component.getNextPage()
+    expect(spyFn).toHaveBeenCalled()
+  })
+  it('getNextPage should be called', () => {
+    const fetchFlat$ = {
+      info: 'string',
+      street: 'string',
+      price: 'string',
+      img: 'string'
+    }
+    component.nextUrl = ''
+    const spyFn = spyOn(component.httpService, 'getTheApiNext').and.returnValue(of(fetchFlat$))
+    component.getNextPage()
+    expect(spyFn).not.toHaveBeenCalled()
   })
 })

f3b5c3710db9ad32838c91d901153cd0dafdf577
Renszo Camacho <varsovia84@gmail.com>
2021-05-26 16:41:37 +0200

    Import registerLocaleData - Import es from ...locales/es

--- Gio-camacho/week-5/hola-pisos/src/app/card/card.component.ts
+++ Gio-camacho/week-5/hola-pisos/src/app/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core'
 import { HolapisosService } from '../services/holapisos.service'
 import { environment } from '../../environments/environment'
+import { registerLocaleData } from '@angular/common'
+import es from '@angular/common/locales/es'
 
 @Component({
   selector: 'app-card',
@@ -12,12 +14,14 @@ export class CardComponent implements OnInit {
   next!: string
   currency: string = '€'
   previous!: string
+  moreInfo:Array<{}> = []
 
   // eslint-disable-next-line no-useless-constructor
   constructor (public httpServices: HolapisosService) { }
 
   ngOnInit (): void {
     this.fetch(environment.url)
+    registerLocaleData(es)
   }
 
   fetch (url:string) {
@@ -26,30 +30,23 @@ export class CardComponent implements OnInit {
         (res:any) => {
           this.cardData = res.data
           this.next = res.links.next.href
+          this.moreInfo.push(...res.data)
           obs$.unsubscribe()
         }
       )
   }
 
-  nextPage () {
+  showMore () {
     if (this.next) {
       this.httpServices.getData(this.next).subscribe((res:any) => {
         this.next = res.links.next.href
         this.previous = res.links.prev.href
-        this.cardData = res.data
+        // this.tito.push(...this.cardData)
+        this.moreInfo.push(...res.data)
+        this.cardData = this.moreInfo
+        // console.log(this.cardData)
+        console.log(this.moreInfo)
       })
-    }
-  }
-
-  previousPage () {
-    if (this.previous !== environment.startUrl) {
-      this.httpServices.getData(this.previous).subscribe((res:any) => {
-        this.next = res.links.next.href
-        this.previous = res.links.prev.href
-        this.cardData = res.data
-      })
-    } else {
-      this.ngOnInit()
     }
   }
 }

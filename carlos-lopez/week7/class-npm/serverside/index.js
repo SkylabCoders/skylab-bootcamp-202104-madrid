a2f4401a995afcdcb0e6f8e98b5f303fa9154188
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-27 16:27:07 +0200

    Print variables from a form

--- carlos-lopez/week7/class-npm/serverside/index.js
+++ carlos-lopez/week7/class-npm/serverside/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
@@ -8,4 +10,15 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about')
 })
+app.post('/home', (req, res) => {
+  console.log(req.body)
+  res.render('home', {
+    name: req.body.name,
+    surname: req.body.surname
+  })
+})
+app.get('/form', (req, res) => {
+  res.render('form')
+})
+
 app.listen(4000, () => console.log('Server is running in http://localhost:4000'))

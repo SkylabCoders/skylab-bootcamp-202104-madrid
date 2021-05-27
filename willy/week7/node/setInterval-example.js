646e18c06cd539eda6ffbcba098a677e94da0a72
Willy <acropolis_arcaica@hotmail.com>
2021-05-27 13:04:23 +0200

    Add challenges

--- willy/week7/node/setInterval-example.js
+++ willy/week7/node/setInterval-example.js
@@ -1,5 +1,27 @@
 // setInterval(() => console.log('Hello every 4 secs'), 4000)
 
-const timerId = setInterval(() => console.log('Hello'), 0);
+// const timerId = setInterval(() => console.log('Hello'), 0);
 
-clearInterval(timerId)
+// clearInterval(timerId)
+
+/**
+ * Print hello world
+ * every second
+ * and stop after 5 times, then print Done
+ */
+
+let times = 0
+const interval = setInterval(() => {
+    console.log('hola')
+    times++;
+
+    if (times ===5){
+        console.log('done')
+        clearInterval(interval)
+    }
+},1000)
+
+  
+
+
+

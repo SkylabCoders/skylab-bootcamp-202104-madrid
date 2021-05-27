a45aec44c742bdb409c5c2552854ff8617d3a9b9
Adriancit0 <adrianruanol@gmail.com>
2021-05-27 09:13:06 +0200

    Fix pagination Add pipe to price paragraph

--- adrian/Curso/week-6/prueba-tecnica/src/app/app.component.ts
+++ adrian/Curso/week-6/prueba-tecnica/src/app/app.component.ts
@@ -4,7 +4,6 @@
 /* eslint-disable no-empty-function */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable import/prefer-default-export */
-
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from './services/http.service';
 
@@ -16,32 +15,38 @@ import { HttpService } from './services/http.service';
 export class AppComponent implements OnInit {
   title = 'prueba-tecnica';
 
-houses:any
-
-url = 'https://apidev.holapisos.com/es/api/node/inmuebles?page[limit]=32&page[offset]=0&sort[weight][path]=field_inmu_peso_orde&sort[weight][direction]=DESC&sort[relevancy][path]=relevancy&sort[relevancy][direction]=DESC&fields[node--inmuebles]=field_inmu_imag_arra,field_inmu_prec,field_inmu_nume_habi,field_inmu_refe,field_inmu_pobl,field_inmu_area_cons,field_inmu_tipo_sin_agru,field_inmu_tipo_via,field_inmu_nomb_call';
+houses:Array<any> = [];
+limitValue = 200;
+url = `https://apidev.holapisos.com/es/api/node/inmuebles?page[limit]=32&page[offset]=0&sort[weight][path]=field_inmu_peso_orde&sort[weight][direction]=DESC&sort[relevancy][path]=relevancy&sort[relevancy][direction]=DESC&fields[node--inmuebles]=field_inmu_imag_arra,field_inmu_prec,field_inmu_nume_habi,field_inmu_refe,field_inmu_pobl,field_inmu_area_cons,field_inmu_tipo_sin_agru,field_inmu_tipo_via,field_inmu_nomb_call`;
+results:any
 
 constructor(public httpService: HttpService) { }
 
 ngOnInit() {
-  this.httpService.fetchApi(this.url).subscribe((res) => {
-    this.houses = res;
-  });
+  this.updateFetch()
 }
 
+
 updateFetch() {
-  this.httpService.fetchApi(this.url).subscribe((res) => {
-    this.houses = res;
+  this.httpService.fetchApi(this.url).subscribe((res:any) => {
+    res.data.forEach((house:any) => {
+      this.results = res;
+      this.houses.push(house)
+    });
   });
 }
 
 getNext() {
-  if (this.houses.links.next) { this.url = this.houses.links.next.href; }
+  if (this.results.links.next) {
+    this.url = this.results.links.next.href;
+  }
   this.updateFetch();
+  
 }
 
 getPrevius() {
-  if (this.houses.links.prev) {
-    this.url = this.houses.links.prev.href;
+  if (this.results.links.prev) {
+    this.url = this.results.links.prev.href;
     this.updateFetch();
   }
 }
--- adrian/Curso/week-6/prueba-tecnica/src/app/app.module.ts
+++ adrian/Curso/week-6/prueba-tecnica/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ChangeNumberPipe } from './pipes/change-number.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChangeNumberPipe
   ],
   imports: [
     BrowserModule,

d5da1d03de6e798dd2a317702620d6eee44c7357
Aday <aday69@hotmail.com>
2021-05-28 16:23:21 +0200

    Add changes in routes

--- aday/Challenges-Course/week7/ejs-firebase/routes/router.js
+++ aday/Challenges-Course/week7/ejs-firebase/routes/router.js
@@ -1,6 +1,20 @@
 const index = require('../index')
 const express = require('express')
 
+const firebase = require('firebase')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcm0o6IwR8N7WJ2BqdvOLPJ200cZPf8ZI",
+    authDomain: "fir-ejs-a5765.firebaseapp.com",
+    databaseURL: "https://fir-ejs-a5765-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fir-ejs-a5765",
+    storageBucket: "fir-ejs-a5765.appspot.com",
+    messagingSenderId: "674455607548",
+    appId: "1:674455607548:web:4f87f4f43d8923e8cb9f79"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
 function router() {
 const routes = express.Router()
 
@@ -11,13 +25,19 @@ routes
 })
 
 routes
-.route('/form')
+.route('/register')
 .get((req, res) => {
-    res.render('form')
+    res.render('register')
 })
 
 routes
-.route('/home')
+.route('/login')
+.get((req, res) => {
+    res.render('login')
+})
+
+routes
+.route('/welcome')
 .post((req, res) => {
     const newUser = {
         name: req.body.name,
@@ -27,7 +47,28 @@ routes
     res.send('Has sido registrado correctamente!')
 })
 
+routes
+.route('/home')
+.post((req, res) => {
+    const { username, password } = req.body
+ 
+     res.send('Te has logueado correctamente')
+
+      firebase.auth().signInWithEmailAndPassword(username, password)
+        .then((userCredential) => {
+        // Signed in
+          const { user }  = userCredential
+        // ...
+        })
+        .catch((error) => {
+            console.log(error)
+          const { errorCode } = error
+          const { errorMessage } = error
+        })
+})
+
 return routes
 }
 
 module.exports = router()
+

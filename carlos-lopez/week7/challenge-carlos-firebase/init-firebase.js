14e5367289e33c4f36392b7ed768830b99f57e30
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-28 10:09:33 +0200

    Delete console.logs

--- carlos-lopez/week7/challenge-carlos-firebase/init-firebase.js
+++ carlos-lopez/week7/challenge-carlos-firebase/init-firebase.js
@@ -12,33 +12,27 @@ const firebaseConfig = {
   messagingSenderId: '363508471283',
   appId: '1:363508471283:web:55134182c2cd6e5b342e86'
 }
+firebase.initializeApp(firebaseConfig)
 
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('index')
 })
 app.post('/home', (req, res) => {
-  console.log(req.body)
   res.render('home', {
     name: req.body.name,
     surname: req.body.surname
   })
-  console.log(firebase.initializeApp(firebaseConfig))
 
   firebase.auth().signInWithEmailAndPassword(req.body.name, req.body.surname)
     .then((userCredential) => {
     // Signed in
-    console.log(userCredential)
       const user = userCredential.user
-      console.log('Log in' + user)
-      console.log('Esto es lo que buscas ' + userCredential.user + '*********')
     // ...
     })
     .catch((error) => {
       const errorCode = error.code
       const errorMessage = error.message
-      console.log(errorCode)
-      console.log(errorMessage)
     })
 })
 // Your web app's Firebase configuration

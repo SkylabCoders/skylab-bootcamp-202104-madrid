dc73dea2379e2140cca6f504b9f81e63280fee4c
Willy <acropolis_arcaica@hotmail.com>
2021-05-29 19:18:42 +0200

    Modify heroRoutes

--- willy/week7/toh-react-fullstack/backend/routes/heroRoutes.js
+++ willy/week7/toh-react-fullstack/backend/routes/heroRoutes.js
@@ -1,18 +1,19 @@
 const {Router} = require('express')
+const {getAllHeroes, getById, createHero, updateById, deleteById} = require('./../controllers/heroController')
 
 function heroRouter () {
     const heroRoutes = Router()
 
     heroRoutes
     .route('/')
-    .get()
-    .post()
+    .get(getAllHeroes)
+    .post(createHero)
 
     heroRoutes
     .route('/:heroId')
-    .get()
-    .put()
-    .delete()
+    .get(getById)
+    .put(updateById)
+    .delete(deleteById)
 }
 
 module.exports = heroRoutes();

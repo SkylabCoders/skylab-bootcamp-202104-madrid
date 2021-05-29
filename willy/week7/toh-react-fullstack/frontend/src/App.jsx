414fc1180d5e502ba56e132280ba8139a0376d81
Willy <acropolis_arcaica@hotmail.com>
2021-05-29 13:19:51 +0200

    Update App

---
+++ willy/week7/toh-react-fullstack/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const saludo = 'hola react';
+
+  return (
+
+    <div className="App">
+      <p>{saludo}</p>
+    </div>
+
+  );
+}
+
+export default App;

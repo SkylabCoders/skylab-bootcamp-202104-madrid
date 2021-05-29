af08ac8ff31368171e31a2df16375df522d698b7
Renszo Camacho <varsovia84@gmail.com>
2021-05-29 20:51:59 +0200

    Add Link component from react-router-dom - Create a navbar

--- Gio-camacho/week-5/ToH-React-Fullstack/frontend/src/App.jsx
+++ Gio-camacho/week-5/ToH-React-Fullstack/frontend/src/App.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
+import List from './components/List/List';
 
 function App() {
   return (
     <Router>
+      <h1>Tour of Heroes</h1>
+      <nav>
+        <Link to="/">Dashboard</Link>
+        <Link to="list">List</Link>
+      </nav>
       <Switch>
         <Route path="/" exact component={Dashboard} />
+        <Route path="/list" exact component={List} />
       </Switch>
     </Router>
   );

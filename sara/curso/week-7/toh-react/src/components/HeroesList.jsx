931c943c4c95ec877e7515983cc0991696c40445
Sara Cubero <cuberosara@gmail.com>
2021-05-30 19:49:54 +0200

    Create list component

---
+++ sara/curso/week-7/toh-react/src/components/HeroesList.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import heroes from '../data/heroesData';
+
+function HeroesList() {
+  const completeList = heroes.map((heroe) => (
+    <li key={heroe.id}>
+      <Link to={`/heroesDetail/${heroe.id}`}>
+        {heroe.name}
+      </Link>
+    </li>
+  ));
+  return (
+    <ul>
+      {completeList}
+    </ul>
+  );
+}
+export default HeroesList;

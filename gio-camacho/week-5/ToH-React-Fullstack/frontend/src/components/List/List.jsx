b3fe43910c554bd578575b45d3d3efc28e019f7f
Renszo Camacho <varsovia84@gmail.com>
2021-05-30 13:09:14 +0200

    Render all heroes in the list

--- gio-camacho/week-5/ToH-React-Fullstack/frontend/src/components/List/List.jsx
+++ gio-camacho/week-5/ToH-React-Fullstack/frontend/src/components/List/List.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function List() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4002/api/heroes/')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((newData) => {
+        setData(newData);
+      })
+      .catch((newError) => {
+        setError(newError);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return 'Loading...';
+  if (error) return 'Error!';
   return (
     <div>
       <h2>My Heroes</h2>
+      {
+        data.map((hero) => (
+          <p
+            // eslint-disable-next-line no-underscore-dangle
+            key={hero._id}
+          >
+            {hero.name}
+          </p>
+        ))
+      }
     </div>
   );
 }

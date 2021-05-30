0ede01f5e1655a2dd4c35d8611a136d3bd686596
Willy <acropolis_arcaica@hotmail.com>
2021-05-30 12:58:08 +0200

    Refactor list.jsx

--- willy/week7/toh-react-fullstack/frontend/src/components/list/List.jsx
+++ willy/week7/toh-react-fullstack/frontend/src/components/list/List.jsx
@@ -1,12 +1,4 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
-// eslint-disable-next-line import/no-cycle
-import Dashboard from '../dashboard/Dashboard';
 
 function List() {
   return (
@@ -14,14 +6,6 @@ function List() {
       <div className="list">
         Estas en lista
       </div>
-      <Router>
-        <Link to="/dashboard">Dashboard</Link>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Switch>
-      </Router>
     </>
   );
 }

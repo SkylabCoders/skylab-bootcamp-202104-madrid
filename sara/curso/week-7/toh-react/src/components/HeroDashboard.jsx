7ab0ae6862768ddd10ba852f5296d83e95d337bb
Sara Cubero <cuberosara@gmail.com>
2021-05-30 19:48:47 +0200

    Create Dashboard component

---
+++ sara/curso/week-7/toh-react/src/components/HeroDashboard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import heroes from '../data/heroesData';
+
+function HeroesDashboard() {
+  let listHeroes = [...heroes];
+  listHeroes = listHeroes.sort(() => Math.random() - 0.5);
+  const randomHeroes = listHeroes.slice(0, 4);
+  const listToRender = randomHeroes.map((heroe) => (
+    <li key={heroe.id}>
+      <Link to={`/heroesDetail/${heroe.id}`}>
+        {heroe.name}
+      </Link>
+    </li>
+  ));
+  return (
+    <ul>
+      {listToRender}
+    </ul>
+  );
+}
+
+export default HeroesDashboard;

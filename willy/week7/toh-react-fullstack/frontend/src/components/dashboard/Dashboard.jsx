6f5a8726cf8af505ee32957e237e3caa32c05b8b
Willy <acropolis_arcaica@hotmail.com>
2021-05-30 12:57:41 +0200

    Add fetch function in dashboard

--- willy/week7/toh-react-fullstack/frontend/src/components/dashboard/Dashboard.jsx
+++ willy/week7/toh-react-fullstack/frontend/src/components/dashboard/Dashboard.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
-// eslint-disable-next-line import/no-cycle
-import List from '../list/List';
+import React, { useState, useEffect } from 'react';
 
 function dashboard() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/heroes/')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((newData) => {
+        setData(newData);
+      })
+      .catch((newError) => {
+        setError(newError);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return 'Loading...';
+  if (error) return 'Error!';
+
   return (
+
     <>
-      <div className="dashboard">
-        dashboard works! react shit!
-      </div>
-      <Router>
-        <Link to="/list">list</Link>
-        <Switch>
-          <Route path="/list">
-            <List />
-          </Route>
-        </Switch>
-      </Router>
+      <h2>
+        Top Heroes
+      </h2>
+      {
+        data.slice(0, 4).map((hero) => (
+          <p
+            // eslint-disable-next-line no-underscore-dangle
+            key={hero._id}
+          >
+            {hero.name}
+          </p>
+        ))
+      }
     </>
   );
 }

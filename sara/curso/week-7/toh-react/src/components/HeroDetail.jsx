c1d052785d339f330b2d0b952888a9f5c9a593e8
Sara Cubero <cuberosara@gmail.com>
2021-05-30 19:49:24 +0200

    Create Detail commponent

---
+++ sara/curso/week-7/toh-react/src/components/HeroDetail.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useParams } from 'react-router';
+import heroes from '../data/heroesData';
+
+function HeroesDetail() {
+  const { heroId } = useParams();
+  const currentHero = heroes.find((hero) => hero.id === heroId);
+  return (
+    <ul>
+      <li>
+        <h1>{currentHero.id}</h1>
+        <h2>{currentHero.name}</h2>
+      </li>
+    </ul>
+  );
+}
+export default HeroesDetail;

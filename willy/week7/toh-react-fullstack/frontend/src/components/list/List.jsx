943d00ae5bc6b736b6a957b61b31bd15c260e631
Willy <acropolis_arcaica@hotmail.com>
2021-05-30 13:08:39 +0200

    Add fetch function in list

--- willy/week7/toh-react-fullstack/frontend/src/components/list/List.jsx
+++ willy/week7/toh-react-fullstack/frontend/src/components/list/List.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function List() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/heroes/')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((newData) => {
+        setData(newData);
+      })
+      .catch((newError) => {
+        setError(newError);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return 'Loading...';
+  if (error) return 'Error!';
   return (
     <>
-      <div className="list">
-        Estas en lista
-      </div>
+
+      {data.map((hero) => (
+        <p
+            // eslint-disable-next-line no-underscore-dangle
+          key={hero._id}
+        >
+          {hero.name}
+        </p>
+      ))}
     </>
   );
 }

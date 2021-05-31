d509f215842202ed1d901c9f195cc478e57f65b1
Adan <chema6822@gmail.com>
2021-05-31 15:20:15 +0200

    Add first react class -Set up and fix eslint issues in react -Create example components -Comunicate children components with father or mother

--- Adan/react-demo/src/index.js
+++ Adan/react-demo/src/index.js
@@ -2,17 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
+import Sum from './components/Sum';
+import Clicker from './components/Clicker';
+
+const clickCallback = (letter) => console.log(letter);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <Hello name="Gilbe">
+      <p>Esto es un elemento hijo</p>
+      <p>Esto es otro elemento</p>
+    </Hello>
+    <Sum a={5} b={4} />
+    <Clicker handleClick={clickCallback} />
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

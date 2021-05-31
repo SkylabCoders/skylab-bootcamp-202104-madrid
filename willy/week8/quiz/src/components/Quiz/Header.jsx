48050f542ea12cd8e9c87e4f7de6fe70be9185f0
Willy <acropolis_arcaica@hotmail.com>
2021-05-31 18:14:54 +0200

    Create Header component

---
+++ willy/week8/quiz/src/components/Quiz/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Author Quiz</h1>
+      <h2>Select the book written by the author shown</h2>
+    </header>
+  );
+}
+
+export default Header;

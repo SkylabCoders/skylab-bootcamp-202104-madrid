a8d6b69097ceeae7b1674eb4feb0ac1662dcca6a
Willy <acropolis_arcaica@hotmail.com>
2021-05-31 18:14:41 +0200

    Create index

---
+++ willy/week8/quiz/src/components/Quiz/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+// import Book from './Book';
+
+function Quiz() {
+  return (
+    <section>
+      <Header />
+      {/* <Book /> */}
+    </section>
+  );
+}
+
+export default Quiz;

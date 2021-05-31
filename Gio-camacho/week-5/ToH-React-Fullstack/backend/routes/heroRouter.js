3e7b7f92e19f298b032b793bcbc88491a5fc56b9
Renszo Camacho <varsovia84@gmail.com>
2021-05-31 09:08:34 +0200

    Generate controller and models

---
+++ Gio-camacho/week-5/ToH-React-Fullstack/backend/routes/heroRouter.js
@@ -0,0 +1,24 @@
+
+const { Router } = require('express')
+const {
+  getAllHeroes, createHero, getHeroById, updateHeroById, deleteHeroById
+} = require('../controllers/callback')
+
+function heroRouter () {
+  const heroRoutes = Router()
+
+  heroRoutes
+    .route('/')
+    .get(getAllHeroes)
+    .post(createHero)
+
+  heroRoutes
+    .route('/:heroId')
+    .get(getHeroById)
+    .put(updateHeroById)
+    .delete(deleteHeroById)
+
+  return heroRoutes
+}
+
+module.exports = heroRouter()

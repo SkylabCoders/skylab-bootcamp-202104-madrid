bcf132c44e3fb0de369eef9f50225263ce41785b
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-05-31 15:19:24 +0200

    Get some examples for rendering

--- carlos-lopez/week8/react-demo/src/index.js
+++ carlos-lopez/week8/react-demo/src/index.js
@@ -1,14 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
+import Sum from './components/Sum';
+import Clicker from './components/Clicker';
+
+const clickCallback = (letter) => console.log(letter);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <Hello name="Carlos">
+      <p>Esto es un elemento hijo</p>
+      <span>Este e sotro hijo</span>
+    </Hello>
+    <Sum a={5} b={4} />
+    <Clicker handleClick={clickCallback} />
+  </>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function

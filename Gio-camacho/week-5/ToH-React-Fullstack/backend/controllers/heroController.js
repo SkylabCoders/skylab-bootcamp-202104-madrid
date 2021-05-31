0babef3a7c43e7fdbfb28c96e75669db8ebbf81d
Renszo Camacho <varsovia84@gmail.com>
2021-05-31 09:09:36 +0200

    Create heroController

---
+++ Gio-camacho/week-5/ToH-React-Fullstack/backend/controllers/heroController.js
@@ -0,0 +1,73 @@
+
+const debug = require('debug')('server:callback')
+const Hero = require('../models/heroModel')
+
+debug('haaa')
+function controller () {
+  const getAllHeroes = async (req, res) => {
+    const query = { ...res.query }
+
+    try {
+      const allHeroes = await Hero.find(query)
+      res.json(allHeroes)
+    } catch (error) {
+      res.status(500)
+      res.send(error)
+    }
+  }
+
+  const createHero = async (req, res) => {
+    try {
+      const newHero = new Hero({
+        ...req.body
+      })
+
+      await newHero.save() /* Promesa */
+      res.json(newHero)
+    } catch (error) {
+      res.status(500)
+      res.send(error)
+    }
+  }
+
+  const getHeroById = async (req, res) => {
+    const { heroId } = req.params
+
+    const hero = await Hero.findById(heroId)
+
+    res.json(hero)
+  }
+
+  const updateHeroById = async (req, res) => {
+    const { heroId } = req.params
+
+    const dataToUpdate = req.body
+
+    const heroUpdate = await Hero.findByIdAndUpdate(
+      heroId,
+      dataToUpdate,
+      { new: true }
+    )
+
+    res.json(heroUpdate)
+  }
+
+  const deleteHeroById = async (req, res) => {
+    const { heroId } = req.params
+
+    await Hero.findByIdAndDelete(heroId)
+
+    res.status(204)
+    res.json()
+  }
+
+  return {
+    getAllHeroes,
+    createHero,
+    getHeroById,
+    updateHeroById,
+    deleteHeroById
+  }
+}
+
+module.exports = controller()

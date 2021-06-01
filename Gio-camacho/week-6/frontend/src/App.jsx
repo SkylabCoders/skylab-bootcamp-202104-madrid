bdaf2251dbb5ae076d9b68a812d9541e7813a4ba
Renszo Camacho <varsovia84@gmail.com>
2021-06-01 18:05:16 +0200

    Create Form component - Generate route to Form component

--- Gio-camacho/week-6/frontend/src/App.jsx
+++ Gio-camacho/week-6/frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Form from './components/Form';
 
 function App() {
   return (
-    <h1>Hola</h1>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/form" component={Form} />
+      </Switch>
+    </Router>
   );
 }
 

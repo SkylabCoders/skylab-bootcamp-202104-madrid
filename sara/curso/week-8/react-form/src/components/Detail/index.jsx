4741aa31a1164fbab3fc84d734e3c2106bce459c
Sara Cubero <cuberosara@gmail.com>
2021-06-01 18:09:04 +0200

    Add Links in components

---
+++ sara/curso/week-8/react-form/src/components/Detail/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Detail() {
+  return (
+    <div>Hola Detail</div>
+  );
+}
+
+export default Detail;
---
+++ sara/curso/week-8/react-form/src/components/Form/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <div>Hola Form</div>
+  );
+}
+
+export default Form;

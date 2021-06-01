c98cb6d4289e1aa6e05ca24005316fbbec9a3a4a
Aday <aday69@hotmail.com>
2021-06-01 09:07:36 +0200

    Change folder file

---
+++ aday/Challenges-Course/week8/the-quiz/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  // eslint-disable-next-line react/jsx-filename-extension
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+reportWebVitals();

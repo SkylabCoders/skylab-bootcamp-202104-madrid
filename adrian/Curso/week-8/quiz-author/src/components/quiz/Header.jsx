765524b90e5a9498dd311aa9a40e2897ca83965b
Adriancit0 <adrianruanol@gmail.com>
2021-06-01 12:01:20 +0200

    Delete useless code

---
+++ adrian/Curso/week-8/quiz-author/src/components/quiz/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Author Quiz</h1>
+      <h2>Select the book writen by the author shown</h2>
+    </header>
+  );
+}
+
+export default Header;

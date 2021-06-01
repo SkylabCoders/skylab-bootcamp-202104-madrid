4b482d09c31e9ef314ac9fbdabd770ba18240d05
Willy <acropolis_arcaica@hotmail.com>
2021-06-01 20:46:35 +0200

    Create header.jsx

---
+++ willy/week8/airfrance/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <h1>AirFrance</h1>
+      <h2>Best prices for worst flights</h2>
+    </div>
+  );
+}
+
+export default Header;

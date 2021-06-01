ea71b88cabebf0d3aa66f32a958a5789cb64ddaf
Renszo Camacho <varsovia84@gmail.com>
2021-06-01 23:55:09 +0200

    Pass data from Form component to Details component

--- Gio-camacho/week-6/frontend/src/components/Details/index.jsx
+++ Gio-camacho/week-6/frontend/src/components/Details/index.jsx
@@ -1,10 +1,19 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { useLocation } from 'react-router';
 
 function Details() {
+  const location = useLocation();
+  const {
+    name,
+    surname,
+    flight,
+  } = location.state;
   return (
     <div>
-      <h1>Hola Details</h1>
+      <h1>Details</h1>
+      <p>{`Bienvenido ${name} ${surname}`}</p>
+      <p>{`Ha reservado un vuelo a ${flight}`}</p>
     </div>
   );
 }

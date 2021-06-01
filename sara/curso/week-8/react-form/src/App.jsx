cd7f5c45c8c7614d0b4915ed8cebdf6459bbface
Sara Cubero <cuberosara@gmail.com>
2021-06-01 17:59:37 +0200

    Create Routes

--- sara/curso/week-8/react-form/src/App.jsx
+++ sara/curso/week-8/react-form/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Form from './components/Form';
+import Detail from './components/Detail';
 
 function App() {
   return (
-    <div className="App">
-      Hola
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/form" component={Form} />
+        <Route exact path="/detail" component={Detail} />
+      </Switch>
+    </Router>
   );
 }
 

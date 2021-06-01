fc4075cc3faca5bc0962f0043ffd86a8a4a4943f
Aday <aday69@hotmail.com>
2021-06-01 09:25:11 +0200

    Fix problems in index

--- aday/Challenges-Course/week8/react-demo/src/index.js
+++ aday/Challenges-Course/week8/react-demo/src/index.js
@@ -3,20 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Quiz from './components/Quiz';
+import Hello from './components/Hello';
+import Sum from './components/Sum';
+import Clicker from './components/Clicker';
 
-ReactDOM.render(
-  <Quiz />,
-  document.getElementById('root'),
-);
-
-reportWebVitals();
-// import Hello from './components/Hello';
-// import Sum from './components/Sum';
-// import Clicker from './components/Clicker';
-
-// eslint-disable-next-line no-alert
-/* C L A S E :
 const clickCallback = (letter) => alert(letter);
 
 ReactDOM.render(
@@ -32,4 +22,6 @@ ReactDOM.render(
     <Clicker handleClick={(clickCallback)} />
   </>,
   document.getElementById('root'),
-); */
+);
+
+reportWebVitals();

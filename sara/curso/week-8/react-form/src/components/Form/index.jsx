58dd16fc10b3caacd5b57554cedb936c0e609a5a
Sara Cubero <cuberosara@gmail.com>
2021-06-01 18:22:32 +0200

    Create first version Form

--- sara/curso/week-8/react-form/src/components/Form/index.jsx
+++ sara/curso/week-8/react-form/src/components/Form/index.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+  const [name, setName] = useState('');
+  const [send, setSend] = useState(false);
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSend(true);
+  }
   return (
-    <div>Hola Form</div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Frirst Name:
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {send
+      && (
+        <h1>
+          Hola
+          {' '}
+          {name}
+        </h1>
+      )}
+    </>
   );
 }
 

e64f4863ee1192930335070e2c627e5b64a6479a
Willy <acropolis_arcaica@hotmail.com>
2021-06-01 09:25:37 +0200

    Add array method in index.jsx

--- willy/week8/quiz/src/components/Quiz/index.jsx
+++ willy/week8/quiz/src/components/Quiz/index.jsx
@@ -1,13 +1,42 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/prop-types */
 import React from 'react';
-import Header from './Header';
-// import Book from './Book';
+import Header from './Header/Header';
+import Book from './Book';
+
+import bookData from '../../constants/bookData';
 
 function Quiz() {
   return (
-    <section>
+
+    <div>
+
       <Header />
-      {/* <Book /> */}
-    </section>
+      <section>
+        <ul>
+
+          { bookData.splice(0, 4).map((book) => (
+
+            <Book
+              title={book.title}
+              key={book.title}
+            />
+          ))}
+
+        </ul>
+      </section>
+      {/* TODO <div>
+        { isCorrect && (
+        <button
+          type="button"
+          onClick={() => { nextQuiz(); setIsCorrect(null); }}
+        >
+          Next quiz
+        </button>
+        )}
+      </div> */}
+    </div>
+
   );
 }
 

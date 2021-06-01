32463739dd8dd82695e2842d76f807a576995b1e
Sara Cubero <cuberosara@gmail.com>
2021-06-01 16:26:45 +0200

    Add test utils

--- sara/curso/week-8/test-react/react-demo/src/utils/utils.test.js
+++ sara/curso/week-8/test-react/react-demo/src/utils/utils.test.js
@@ -1,25 +1,11 @@
-const { getRandomArbitrary, getFourRandomValues, nextQuiz } = require('./utils');
-
-global.Math = {
-  random: jest.fn(),
-  floor: jest.fn(),
-};
+const { nextQuiz } = require('./utils');
 
 describe('in utils', () => {
-  describe('given a getRandomArbitrary function', () => {
-    describe('When calling it passing two numbers', () => {
-      let min;
-      let max;
-      beforeEach(() => {
-        min = 2;
-        max = 10;
-        getRandomArbitrary(max, min);
-      });
-      test('It should call random method', () => {
-        expect(Math.random).toHaveBeenCalled();
-      });
-      test('It should call floor method', () => {
-        expect(Math.floor).toHaveBeenCalled();
+  describe('given a nextQuiz function', () => {
+    describe('When it is invoked', () => {
+      test('should return {books, title, name, authorImg}', () => {
+        const quizData = nextQuiz();
+        expect(Object.keys(quizData)).toEqual(['books', 'author', 'title', 'authorImg']);
       });
     });
   });

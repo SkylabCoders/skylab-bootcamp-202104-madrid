2751b4d35bb41c17c4ee7fddfd37d254858d057e
Adan <chema6822@gmail.com>
2021-06-01 08:07:57 +0200

    Setup environment to work along with eslint

---
+++ Adan/react-quiz-adan/src/components/quiz/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Header from '../header';
+import Book from '../Book';
+import books from '../../constants/books';
+
+export default function Quiz() {
+  const [imgSrc, setImgSrc] = useState(books[books.length - 1].authorImg);
+  const [authorName, setAuthorName] = useState(books[books.length - 1].author);
+  const [correctAnswer, setCorrectAnswer] = useState(books[books.length - 1].title);
+  function validateAnswer(title) {
+    if (title === correctAnswer) {
+      setCorrectAnswer(true);
+    }
+    setCorrectAnswer(false);
+  }
+  return (
+    <>
+      <Header />
+      <section>
+        <img src={imgSrc} alt={authorName} />
+        <ul>
+          { books.map((book, index) => (
+            <Book
+              validateAnswer={validateAnswer}
+              title={book}
+              key={index}
+            />
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+}
--- Adan/react-quiz-adan/src/index.js
+++ Adan/react-quiz-adan/src/index.js
@@ -1,12 +1,13 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Quiz from './components/quiz';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Quiz />
   </React.StrictMode>,
   document.getElementById('root')
 );

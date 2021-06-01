4a270e6fccb49679e823e10526fed6cdd001eaa6
Willy <acropolis_arcaica@hotmail.com>
2021-06-01 20:49:03 +0200

    Modify index.js
    -Modify package.json
    -Remove app files

--- willy/week8/airfrance/src/index.js
+++ willy/week8/airfrance/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// eslint-disable-next-line import/no-named-as-default
+import Main from './components';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );

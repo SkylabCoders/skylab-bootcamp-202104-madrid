f67bd01444ae156182a3741299ad31bd98a82c0a
Aday <aday69@hotmail.com>
2021-06-01 09:21:50 +0200

    Fix routes problem

--- aday/Challenges-Course/week8/the-quiz/src/index.js
+++ aday/Challenges-Course/week8/the-quiz/src/index.js
@@ -1,14 +1,12 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Quiz from './components/Quiz';
 
 ReactDOM.render(
-  // eslint-disable-next-line react/jsx-filename-extension
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Quiz />,
   document.getElementById('root'),
 );
 

a3c3a5c73d6604ba94b418df698e468baa0476e6
Willy <acropolis_arcaica@hotmail.com>
2021-06-02 16:59:43 +0200

    Modify actionCreator

--- willy/week8/airfrance/src/redux/actions/actionCreator.js
+++ willy/week8/airfrance/src/redux/actions/actionCreator.js
@@ -1,15 +1,20 @@
-import axios from 'axios';
+// import axios from 'axios';
 import actiontypes from './actionTypes';
 
 export function login(email, password) {
-  return async (dispatch) => {
-    const { data } = await axios.post(
-      process.env.REACT_APP_API_URL, { email, password }
-    );
-    dispatch({
-      type: actiontypes.LOGIN,
-      user: data
-    });
+//   return async (dispatch) => {
+//     const { data } = await axios.post(
+//       process.env.REACT_APP_API_URL, { email, password }
+//     );
+//     dispatch({
+//       type: actiontypes.LOGIN,
+//       user: data
+//     });
+//   };
+
+  return {
+    type: actiontypes.LOGIN,
+    user: { email, password }
   };
 }
 
@@ -20,15 +25,20 @@ export function logout() {
 }
 
 export function bookFlight(formData) {
-  return async (dispatch) => {
-    const { data } = await axios.post(
-      process.env.REACT_APP_API_URL,
-      formData
-    );
+//   return async (dispatch) => {
+//     const { data } = await axios.post(
+//       process.env.REACT_APP_API_URL,
+//       formData
+//     );
 
-    dispatch({
-      type: actiontypes.BOOK_FLIGHT,
-      flightData: data
-    });
+  //     dispatch({
+  //       type: actiontypes.BOOK_FLIGHT,
+  //       flightData: data
+  //     });
+  //   };
+
+  return {
+    type: actiontypes.BOOK_FLIGHT,
+    flightData: formData
   };
 }

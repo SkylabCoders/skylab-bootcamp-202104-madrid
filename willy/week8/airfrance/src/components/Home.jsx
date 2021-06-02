6cb8cad32752134d1279aefed5cdedfac4261a2c
Willy <acropolis_arcaica@hotmail.com>
2021-06-02 09:12:31 +0200

    Add home.css

--- willy/week8/airfrance/src/components/Home.jsx
+++ willy/week8/airfrance/src/components/Home.jsx
@@ -3,14 +3,16 @@ import {
   Link
 } from 'react-router-dom';
 import Header from './Header';
+import './home.css';
 
 function Home() {
   return (
-    <div>
+    <>
       <Header />
-      <Link to="/form">Start</Link>
-    </div>
-
+      <div className="container">
+        <Link to="/form">Start</Link>
+      </div>
+    </>
   );
 }
 

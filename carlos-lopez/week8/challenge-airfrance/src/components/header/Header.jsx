baee3a7103a7af2a03ac0cc09c6e1baec3397652
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-06-02 19:42:41 +0200

    Set form for login properly working

--- carlos-lopez/week8/challenge-airfrance/src/components/header/Header.jsx
+++ carlos-lopez/week8/challenge-airfrance/src/components/header/Header.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { login, logout } from '../../redux/actions/actionCreators';
 
 function Header() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function checkAuth() {
+    dispatch(login(name, password, true));
+  }
   return (
-    <div className="p-5 mb-4 bg-light rounded-3">
-      {
+    <>
+      <div className="p-5 mb-4 bg-light rounded-3">
+        {
                 user?.isAuthenticated
                   ? (
                     <>
@@ -31,18 +42,30 @@ function Header() {
                   )
                   : (
                     <>
-                      <button
-                        type="button"
-                        className="btn btn-primary"
-                        onClick={() => dispatch(login('carlos@gmail.com', '1234', true))}
-                      >
-                        Login
-                      </button>
+                      <form onSubmit={handleSubmit}>
+                        <label htmlFor="name">
+                          email:
+                          <input type="text" name="name" onChange={(e) => setName(e.target.value)} value={name} />
+                        </label>
+                        <label htmlFor="password">
+                          Password:
+                          <input type="text" name="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                        </label>
+
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          onClick={checkAuth}
+                        >
+                          Login
+                        </button>
+                      </form>
 
                     </>
                   )
             }
-    </div>
+      </div>
+    </>
   );
 }
 export default Header;

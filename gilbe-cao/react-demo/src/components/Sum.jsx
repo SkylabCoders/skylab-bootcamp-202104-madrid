996fb236745646c7378cdffcec23511c5aa0fd78
Gilberto Cao <gilbe.cao@gmail.com>
2021-06-02 10:12:06 +0200

    Add prop types

--- gilbe-cao/react-demo/src/components/Sum.jsx
+++ gilbe-cao/react-demo/src/components/Sum.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
 function Sum({ a, b }) {
-  return `${a} + ${b} = ${a + b}`;
+  return <h1>{`${a} + ${b} = ${a + b}`}</h1>;
 }
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
+};
 
 export default Sum;

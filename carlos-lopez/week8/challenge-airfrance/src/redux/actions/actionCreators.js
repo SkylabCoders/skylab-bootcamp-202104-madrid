34ad0171b849cf46742359d4a4ec0345f515e19c
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-06-02 16:31:01 +0200

    Config redux

---
+++ carlos-lopez/week8/challenge-airfrance/src/redux/actions/actionCreators.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+export function login(email, password) {
+  return async (dispatch) => {
+    const { data } = await axios.post(process.env.REACT_APP_API_URL, { email, password });
+    dispatch({
+      type: actionTypes.LOGIN,
+      user: data,
+    });
+  };
+}
+export function logout() {
+  return {
+    type: actionTypes.LOGOUT,
+  };
+}
+export function bookFlight(formData) {
+  return async (dispatch) => {
+    const { data } = await axios.post(
+      process.env.REACT_APP_API_URL,
+      formData,
+    );
+    dispatch({
+      type: actionTypes.LOGIN,
+      flightData: data,
+    });
+  };
+}

cd8c49da4cdc6982e2f7471a023681a990ae078f
Gilberto Cao <gilbe.cao@gmail.com>
2021-06-02 12:36:07 +0200

    Configure redux

--- gilbe-cao/react-demo/src/index.js
+++ gilbe-cao/react-demo/src/index.js
@@ -3,21 +3,26 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+import configureStore from './redux/store';
 
 import Quiz from './components/Quiz';
 import Home from './components/Quiz/Home';
 
 ReactDOM.render(
   <div className="container">
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/quiz" component={Quiz} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={configureStore()}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/quiz" component={Quiz} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   </div>,
   document.getElementById('root'),
 );
---
+++ gilbe-cao/react-demo/src/redux/actions/actionTypes.js
@@ -0,0 +1,5 @@
+export default {
+  NEXT_QUIZ: 'NEXT_QUIZ',
+  START_QUIZ: 'START_QUIZ',
+  END_QUIZ: 'END_QUIZ',
+};

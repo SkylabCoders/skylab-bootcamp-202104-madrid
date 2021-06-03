dfd14209639192e28ac233d96c2c113bf9f474e7
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-06-03 15:28:02 +0200

    Set up for app work

---
+++ carlos-lopez/week8/toh-redux-fullstack/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hola amigo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

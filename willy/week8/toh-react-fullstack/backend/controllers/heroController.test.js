3c6196f6f59da8ab8ca5c7de0d74f5ced4dc86ea
Willy <acropolis_arcaica@hotmail.com>
2021-06-03 22:59:04 +0200

    Add heroController

--- willy/week8/toh-react-fullstack/backend/controllers/heroController.test.js
+++ willy/week8/toh-react-fullstack/backend/controllers/heroController.test.js
@@ -111,4 +111,166 @@ describe('heroController', () => {
       });
     });
   });
+
+  describe('Given a getById function', () => {
+    describe('When is invoked', () => {
+      let req;
+      let res;
+
+      describe('And there is no error', () => {
+        beforeEach(async () => {
+          req = {
+            params: null
+          };
+          res = {
+            json: jest.fn()
+          };
+
+          await getById(req, res);
+        });
+
+        test('Then call res.json once', () => {
+          expect(res.json).toHaveBeenCalled();
+        });
+
+        test('Then call Heros.findById', () => {
+          expect(Heros.findById).toHaveBeenCalled();
+        });
+      });
+
+      describe('And there is an error', () => {
+        beforeEach(async () => {
+          req = {
+            body: null
+          };
+          res = {
+            json: jest.fn(),
+            status: jest.fn(),
+            send: jest.fn()
+          };
+
+          Heros.findById.mockRejectedValueOnce('find by id error');
+
+          await getById(req, res);
+        });
+
+        test('Then call  res.status with 500', () => {
+          expect(res.status).toHaveBeenCalledWith(500);
+        });
+
+        test('Then call res.send with find by id error', () => {
+          expect(res.send).toHaveBeenCalledWith('find by id error');
+        });
+      });
+    });
+  });
+
+  describe('Given a updateById function', () => {
+    describe('When is invoked', () => {
+      let req;
+      let res;
+
+      describe('And there is no error', () => {
+        beforeEach(async () => {
+          req = {
+            params: null,
+            body: null
+          };
+          res = {
+            json: jest.fn()
+          };
+
+          await updateById(req, res);
+        });
+
+        test('Then call res.json once', () => {
+          expect(res.json).toHaveBeenCalled();
+        });
+
+        test('Then call Heros.findByIdAndUpdate', () => {
+          expect(Heros.findByIdAndUpdate).toHaveBeenCalled();
+        });
+      });
+
+      describe('And there is an error', () => {
+        beforeEach(async () => {
+          req = {
+            body: null,
+            params: null
+          };
+          res = {
+            json: jest.fn(),
+            status: jest.fn(),
+            send: jest.fn()
+          };
+
+          Heros.findByIdAndUpdate.mockRejectedValueOnce('update error');
+
+          await updateById(req, res);
+        });
+
+        test('Then call  res.status with 500', () => {
+          expect(res.status).toHaveBeenCalledWith(500);
+        });
+
+        test('Then call res.send with update error', () => {
+          expect(res.send).toHaveBeenCalledWith('update error');
+        });
+      });
+    });
+  });
+
+  describe('Given a deleteById function', () => {
+    describe('When is invoked', () => {
+      let req;
+      let res;
+
+      describe('And there is no error', () => {
+        beforeEach(async () => {
+          req = {
+            params: null
+
+          };
+          res = {
+            status: jest.fn()
+          };
+
+          await deleteById(req, res);
+        });
+
+        test('Then call res.status with 204', () => {
+          expect(res.status).toHaveBeenCalledWith(204);
+        });
+
+        test('Then call Heros.findByIdAndUpdate', () => {
+          expect(Heros.findByIdAndDelete).toHaveBeenCalled();
+        });
+      });
+
+      describe('And there is an error', () => {
+        beforeEach(async () => {
+          req = {
+            params: null
+          };
+          res = {
+
+            status: jest.fn(),
+            send: jest.fn()
+          };
+
+          Heros.findByIdAndDelete.mockRejectedValueOnce('delete error');
+
+          await deleteById(req, res);
+        });
+
+        test('Then call  res.status with 500', () => {
+          expect(res.status).toHaveBeenCalledWith(500);
+        });
+
+        test('Then call res.send with update error', () => {
+          expect(res.send).toHaveBeenCalledWith('delete error');
+        });
+      });
+    });
+  });
 });

4bf66f963bc0eceb73fcd775025a0f5919a419de
Adan <chema6822@gmail.com>
2021-06-03 17:32:29 +0200

    Create functions for actionCreators

--- Adan/toh-redux-fullstack/frontend/src/App.jsx
+++ Adan/toh-redux-fullstack/frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect } from 'react';
 
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log(process.env.REACT_APP_API_URL);
+  });
   return (
+
     <div className="App">
       <h1>Hola</h1>
     </div>
---
+++ Adan/toh-redux-fullstack/frontend/src/redux/actions/actionCreators.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import actionTypes from './actionTypes';
+
+const url = process.env.REACT_APP_API_URL;
+export function getHeroes() {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios(url);
+      dispatch({
+        type: actionTypes.GET_HEROES,
+        heroes: data
+      });
+    } catch (error) {
+      dispatch({
+        type: actionTypes.HERO_ERROR
+      });
+    }
+  };
+}
+export function addHero(hero) {
+  return async (dispatch) => {
+    const { data } = await axios.post(url, hero);
+    dispatch({
+      type: actionTypes.POST_HERO,
+      hero: data
+    });
+  };
+}
+export function deleteHero(heroId) {
+  return async (dispatch) => {
+    await axios.delete(`${url}/${heroId}`);
+    dispatch({
+      type: actionTypes.DELETE_HERO,
+      heroId
+    });
+  };
+}
+export function modifyHero(hero) {
+  return async (dispatch) => {
+    const { data } = await axios.put(`${url}/${hero.id}`, hero);
+    dispatch({
+      type: actionTypes.PUT_HERO,
+      hero: data
+    });
+  };
+}
+export function getOneHero(heroId) {
+  // eslint-disable-next-line no-debugger
+  debugger;
+  return async (dispatch) => {
+    const { data } = await axios(`${url}/${heroId}`);
+    dispatch({
+      type: actionTypes.GET_ONE_HERO,
+      hero: data
+    });
+  };
+}

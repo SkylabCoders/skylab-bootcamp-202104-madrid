36bff2d14c50cbe8ba1fc0b508905132927e2b9e
Sara Cubero <cuberosara@gmail.com>
2021-06-03 10:02:18 +0200

    Set up to Protect routes

--- sara/curso/week-8/react-form/src/App.jsx
+++ sara/curso/week-8/react-form/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import Home from './components/Home';
 import Form from './components/Form';
 import Detail from './components/Detail';
 import configureStore from './redux/store';
 import Header from './components/Header';
+import { ProtectedRoute } from './components/protectedRoute';
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/form" component={Form} />
+          <ProtectedRoute exact path="/form" component={Form} />
           <Route exact path="/detail" component={Detail} />
         </Switch>
       </Router>

26d62268cbcbb939709e0bfbbada8ba8ff297060
Renszo Camacho <varsovia84@gmail.com>
2021-06-03 13:23:59 +0200

    Run executer eslint init

--- gio-camacho/week-6/toh-fullstack-react-redux/backend/index.js
+++ gio-camacho/week-6/toh-fullstack-react-redux/backend/index.js
@@ -1,8 +1,7 @@
-const express = require('express')
+const express = require('express');
 
+const app = express();
 
-const app = express()
-
-app.listen(4000, ()=> console.log(
-    'Server is running on port: http://localhost:4000'
-))
+app.listen(4000, () => console.log(
+  'Server is running on port: http://localhost:4000',
+));

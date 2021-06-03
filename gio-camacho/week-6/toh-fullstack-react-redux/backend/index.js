ce2ab2c7c273dcce5151f89968332ba6b03f3fff
Renszo Camacho <varsovia84@gmail.com>
2021-06-03 15:07:44 +0200

    Set the middlewares

--- gio-camacho/week-6/toh-fullstack-react-redux/backend/index.js
+++ gio-camacho/week-6/toh-fullstack-react-redux/backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const debug = require('debug')('app');
 const chalk = require('chalk');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.listen(PORT, () => debug(
   `${chalk.blue('Server')} is ${chalk.magenta('is runnin')} on port ${chalk.cyanBright(`http://localhost:${PORT}`)}`,

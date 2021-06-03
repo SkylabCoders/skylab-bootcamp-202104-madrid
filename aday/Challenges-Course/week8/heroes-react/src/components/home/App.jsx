bdde107c0b05da40a806bc16d77f9cdd8f48f290
Aday <aday69@hotmail.com>
2021-06-03 13:24:11 +0200

    Create components

--- aday/Challenges-Course/week8/heroes-react/src/components/home/App.jsx
+++ aday/Challenges-Course/week8/heroes-react/src/components/home/App.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router, Link, Route, Switch,
+} from 'react-router-dom';
+import DashboardComponent from '../dashboard/Dashboard';
+import DetailsComponent from '../details/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
+    <header className="App-header">
+      <h1>Tour of Heroes</h1>
+      <Router>
+        <div>
+          <Link to="/">Dashboard</Link>
           {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <br />
+          <Link to="/details">Details</Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={DashboardComponent} />
+          <Route exact path="/details" component={DetailsComponent} />
+        </Switch>
+      </Router>
+    </header>
   );
 }
 

3a3dd04d6ab340a7c49294a913ba00b880e8883b
Sara Cubero <cuberosara@gmail.com>
2021-06-03 19:50:23 +0200

    Update HeroeList test

--- sara/curso/week-8/toh-react-redux/src/components/HeroList/HeroList.test.js
+++ sara/curso/week-8/toh-react-redux/src/components/HeroList/HeroList.test.js
@@ -3,14 +3,38 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '../../utils/test-utils';
 import HeroList from './index';
+import { getAll } from '../../redux/actions/actionCreators';
+import actionTypes from '../../redux/actions/actionTypes';
+
+jest.mock('../../redux/actions/actionCreators');
 
 describe('List component ', () => {
   test('should have Dashboard in it', () => {
+    getAll.mockReturnValueOnce({ type: actionTypes.GET_ALL });
     render(
       <MemoryRouter>
         <HeroList />
       </MemoryRouter>,
+      {
+        initialState: {
+          heroes: [{}, { name: 'hero', _id: '11' }],
+        },
+      },
     );
     expect(screen.getByText(/Dashboard/i)).toBeInTheDocument();
+  });
+  test('should have Dashboard in it', () => {
+    getAll.mockReturnValueOnce({ type: '' });
+    render(
+      <MemoryRouter>
+        <HeroList />
+      </MemoryRouter>,
+      {
+        initialState: {
+          heroes: [],
+        },
+      },
+    );
+    expect(getAll).toHaveBeenCalled();
   });
 });

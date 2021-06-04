541c9e00a03ade440ca60ad5f02c93506e5f12f9
Sara Cubero <cuberosara@gmail.com>
2021-06-04 13:25:45 +0200

    Remove router

--- sara/curso/week-8/todo-react/src/App.jsx
+++ sara/curso/week-8/todo-react/src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
-import toDoList from './components/ToDoList';
+import ToDoList from './components/ToDoList';
 import configureStore from './redux/store';
 
 function App() {
   return (
     <Provider store={configureStore()}>
-      <Router>
-        <Route exact path="/" component={toDoList} />
-        <Route exact path="/:taskId" component={toDoList} />
-      </Router>
+      <ToDoList />
     </Provider>
   );
 }

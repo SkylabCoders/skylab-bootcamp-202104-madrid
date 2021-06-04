6415ce851d58f9ea0f25f23b66d5764ec81c1871
Adriancit0 <adrianruanol@gmail.com>
2021-06-04 09:10:32 +0200

    Fix bug in listReducer

--- adrian/Curso/week-8/toh-redux/src/components/heroList/heroList.test.js
+++ adrian/Curso/week-8/toh-redux/src/components/heroList/heroList.test.js
@@ -3,14 +3,39 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import List from './index';
 import { render, screen } from '../../utils/utils';
+import { getAll } from '../../redux/actions/actionCreator';
+import actionTypes from '../../redux/actions/actionTypes';
 
+jest.mock('../../redux/actions/actionCreator');
 describe('Given dashboard component', () => {
   test('Should render LIST', () => {
+    getAll.mockReturnValueOnce({ type: actionTypes.GET_ALL });
     render(
       <MemoryRouter>
         <List />
-      </MemoryRouter>
+      </MemoryRouter>,
+      {
+        initialState: {
+          heroList: [{}, { name: 'adrian', _id: '2' }]
+        }
+      }
+
     );
     expect(screen.getByText(/LIST/i)).toBeInTheDocument();
+  });
+  test('Should call getAll', () => {
+    getAll.mockReturnValueOnce({ type: '' });
+    render(
+      <MemoryRouter>
+        <List />
+      </MemoryRouter>,
+      {
+        initialState: {
+          heroList: []
+        }
+      }
+
+    );
+    expect(getAll).toHaveBeenCalled();
   });
 });

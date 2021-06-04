0906083c10b4a82d482364f76b8c007eda56bc48
Renszo Camacho <varsovia84@gmail.com>
2021-06-04 08:27:28 +0200

    Improve top heroes from Dashboard component - Make four random heroes

--- gio-camacho/week-6/toh-fullstack-react-redux/frontend/src/components/Dashboard/index.jsx
+++ gio-camacho/week-6/toh-fullstack-react-redux/frontend/src/components/Dashboard/index.jsx
@@ -13,7 +13,21 @@ function Dashboard({ heroes, dispatch }) {
     <main className="container my-5">
       <h2>Top Heroes</h2>
       <div className="d-flex flex-column">
-        {heroes.slice(1, 5).map((hero) => <Link className="btn btn-info my-1" to={`/detail/${hero.id}`}>{hero.name}</Link>)}
+        {
+        heroes
+          .sort(() => Math.random() - 0.5)
+          .slice(1, 5)
+          .map(
+            (hero) => (
+              <Link
+                className="btn btn-info my-1"
+                to={`/detail/${hero.id}`}
+              >
+                {hero.name}
+              </Link>
+            ),
+          )
+}
       </div>
     </main>
   );

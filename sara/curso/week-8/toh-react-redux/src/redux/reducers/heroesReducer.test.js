3e5dd8a54d1a96f528c347bd60d258c52ffe407e
Sara Cubero <cuberosara@gmail.com>
2021-06-04 09:38:38 +0200

    Update HeroesReducer test

--- sara/curso/week-8/toh-react-redux/src/redux/reducers/heroesReducer.test.js
+++ sara/curso/week-8/toh-react-redux/src/redux/reducers/heroesReducer.test.js
@@ -1,19 +1,36 @@
 import actionTypes from '../actions/actionTypes';
 import heroesReducer from './heroesReducer';
-import { getAll } from '../actions/actionCreators';
-
-jest.mock('..//actions/actionCreators');
 
 describe('Given heroesReducer', () => {
   describe('When action.type equals actionTypes.GET_ALL ', () => {
     test('return action.heroes', () => {
-      getAll.mockReturnValueOnce({ type: actionTypes.GET_ALL, heroes: 'heroes' });
       const heroes = [];
       const action = {
-        type: '',
+        type: actionTypes.GET_ALL,
+        heroes,
+      };
+      const result = heroesReducer(heroes, action);
+      expect(result).toEqual(action.heroes);
+    });
+  });
+  describe('When action.type equals actionTypes.GET_ALL ', () => {
+    test('return heroes', () => {
+      const heroes = [];
+      const action = {
+        type: 'default case',
+        heroes,
       };
       const result = heroesReducer(heroes, action);
       expect(result).toEqual(heroes);
+    });
+  });
+  describe('When action.type equals actionTypes.GET_ALL ', () => {
+    test('return action.heroes', () => {
+      const action = {
+        type: actionTypes.GET_ALL,
+      };
+      const result = heroesReducer(undefined, action);
+      expect(result).toEqual(action.heroes);
     });
   });
 });

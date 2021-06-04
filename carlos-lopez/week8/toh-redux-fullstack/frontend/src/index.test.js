a36a03fc7984dacf01ed602c489613cc98f691cf
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2021-06-04 09:14:43 +0200

    Delete unnecesary file

--- carlos-lopez/week8/toh-redux-fullstack/frontend/src/index.test.js
+++
@@ -1,8 +0,0 @@
-// import ReactDOM from 'react-dom';
-// import './index';
-
-// describe('test ReactDOM.render', () => {
-//   it('should call ReactDOM.render', () => {
-//     expect(ReactDOM.render).toHaveBeenCalled();
-//   });
-// });

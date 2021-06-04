edb03a611957551062e81ec595c3ff0227eb8bed
Sara Cubero <cuberosara@gmail.com>
2021-06-04 12:14:51 +0200

    Add input to ToDoList Component

--- sara/curso/week-8/todo-react/src/components/ToDoList.jsx
+++ sara/curso/week-8/todo-react/src/components/ToDoList.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getList } from '../redux/actions/actionCreators';
 
 export default function toDoList() {
   const dispatch = useDispatch();
+  const [newTaskValue, setNewTaskValue] = useState('');
   const list = useSelector((store) => store.list);
   useEffect(() => {
     if (!list.length) dispatch(getList());
@@ -11,11 +12,33 @@ export default function toDoList() {
 
   const taskList = list.map((task) => (
     <li>
-      {task.description}
+      <div>
+        {task.description}
+      </div>
+      <button type="button">Update</button>
+      <button type="button">Delete</button>
     </li>
   ));
+  function handleTask(event) {
+    setNewTaskValue(event.target.value);
+  }
+
   return (
     <>
+      <input
+        id="newTaskValue"
+        type="text"
+        value={newTaskValue}
+        onChange={handleTask}
+      />
+      <button
+        type="button"
+        value="Submit"
+        data-testid="login-button"
+      >
+        ADD
+      </button>
+
       <h1>TO DO</h1>
       <ul>{taskList}</ul>
     </>
